12401dca6f8631664c73c7396fea91d1f87e9493
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-04-29 23:47:42 -0400

    Theme
    Creamos el Thema, asignamos que el material3 este activado, y luego le colocamos un color.

--- 03_Counter_app/counter_app/lib/main.dart
+++ 03_Counter_app/counter_app/lib/main.dart
@@ -10,11 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         //Esto es un banner que me da a entender que la app esta en modo desarrollador
         //Con esto quitamos este banner "Debug"
         debugShowCheckedModeBanner: false,
-        //El Scaffold es una implementación de diseño basico de material.
-        home: CounterScreen());
+
+        //Colocamos nuestro Tema
+        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+        //Importamos nuestra screen
+        home: const CounterScreen());
   }
 }
