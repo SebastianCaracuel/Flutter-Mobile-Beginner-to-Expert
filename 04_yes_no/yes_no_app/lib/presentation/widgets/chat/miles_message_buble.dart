2401816555ae83c6a3995c5ac560aeaeaaa998fd
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-04-30 19:10:45 -0400

    Create Other Widget Miles Message
    Creamos el otro Widget, la otra burbuja de mensaje, en este caso la burbuja de Miles.

---
+++ 04_yes_no/yes_no_app/lib/presentation/widgets/chat/miles_message_buble.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MilesMessageBubble extends StatelessWidget {
+  const MilesMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //?! Llamamos a nuestro color del Theme
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      //Llevamos los datos alineados a la izq
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        //Creamos un contenedor
+        Container(
+          //Dejamos una Decoración dentro de nuestro contenedor
+          decoration: BoxDecoration(
+              //?Personalizamos el color dependiendo de nuestro Theme
+              color: colors.secondary,
+              //?No queremos que nuestro contendor sea Cuadrado, queremos que sea redondo.
+              borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola Seba',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+
+        // Todo:imagen
+      ],
+    );
+  }
+}
