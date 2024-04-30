dcc2313136e2cd41e2e480eabcc35fa9605a10c7
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-04-29 23:03:15 -0400

    agregamos scaffold
    Agregamos el Scaffold que estaba en nuestro main a nuestra nueva page "Counter_Screen" para que podamos visualizar la información.

--- 02_First_app_Hello_World/hello_world_app/lib/presentation/screens/counter_screen.dart
+++ 02_First_app_Hello_World/hello_world_app/lib/presentation/screens/counter_screen.dart
@@ -5,6 +5,8 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(child: Text('Hola Mundo')),
+    );
   }
 }
