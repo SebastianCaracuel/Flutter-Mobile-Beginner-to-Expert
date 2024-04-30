ffb1759fb8e03d6da313ebbd59e141cb5244d020
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-04-30 19:06:37 -0400

    Bubble Chat
    Creamos un WIdget que sea lo más parecido a una Burbuja de mensaje. Para eso, primero llamamos al colorScheme, debido a que estamos utilizando un color predeterminado por la lista de colores que creamos.
    
    Luego creamos un contenedor que tiene un boxdecoration, dentro de este boxDecoration nosotros vemos, el color de la burbuja, si queremos que sea redondeada la burbuja o cuadrada.

--- 04_yes_no/yes_no_app/lib/main.dart
+++ 04_yes_no/yes_no_app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         //Quitamos el banner debug (Aplicación en desarrollo)
         debugShowCheckedModeBanner: false,
         //Importamos nuestro tema y aquí es donde podemos cambiar los colores de nuestra app
-        theme: AppTheme(selectedColor: 0).theme(),
+        theme: AppTheme(selectedColor: 2).theme(),
         home: const ChatScreen());
   }
 }
--- 04_yes_no/yes_no_app/lib/presentation/widgets/chat/my_message_buble.dart
+++ 04_yes_no/yes_no_app/lib/presentation/widgets/chat/my_message_buble.dart
@@ -5,6 +5,33 @@ class MyMessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('MyMessage');
+    //?! Llamamos a nuestro color del Theme
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      //Llevamos los datos alineados a la derecha
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        //Creamos un contenedor
+        Container(
+          //Dejamos una Decoración dentro de nuestro contenedor
+          decoration: BoxDecoration(
+              //?Personalizamos el color dependiendo de nuestro Theme
+              color: colors.primary,
+              //?No queremos que nuestro contendor sea Cuadrado, queremos que sea redondo.
+              borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola Mundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        )
+      ],
+    );
   }
 }
