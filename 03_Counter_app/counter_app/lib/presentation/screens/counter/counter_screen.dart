45aaeb3b69b02128adf85e5be9f77240e5f2685e
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-04-29 23:28:08 -0400

    Centramos el Texto
    Centramos el texto utilizando mainAxisAligment, para alinear todo el texto en el centro. Además utilizamos una Columna, debido a que tiene "Childrens" lo que nos permite colocar mas widgets.

--- 03_Counter_app/counter_app/lib/presentation/screens/counter/counter_screen.dart
+++ 03_Counter_app/counter_app/lib/presentation/screens/counter/counter_screen.dart
@@ -6,7 +6,12 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('Counter Mundo')),
-    );
+        body: Center(
+      child: Column(
+        //El mainAxisAligment se utiliza para controlar la alineación de los elementos
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('10'), Text('Cantidad de Clicks')],
+      ),
+    ));
   }
 }
