9c7c0b496726014d5dc8f2ed7c8a72c85c553062
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-01 22:21:43 -0400

    Create Entitie Message
    Creamos dos carpetas nuevas, domain y entities, dentro de entities creamos la entidad del mensaje.

---
+++ 04_yes_no/yes_no_app/lib/domain/entities/message.dart
@@ -0,0 +1,14 @@
+//Creamos una enumeración para tener un control
+enum FromWho { mine, miles }
+
+//Creamos una entidad
+class Message {
+  //Texto
+  final String text;
+  //El meme o gif que me mandará la persona
+  final String? imageUrl;
+  //
+  final FromWho fromWho;
+
+  Message({required this.text, required this.fromWho, this.imageUrl});
+}
