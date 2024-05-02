f8a1f93396350b9eec75da928b20a266b7efaea7
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-01 21:41:20 -0400

    Create Custom decoration for TextFormField
    Creamos decoración para nuestra caja de texto, ademas le agregamos un "Hintext" para que nos de una información de donde debemos colocar nuestro mensaje, y para que sepamos como es la dinamica. También, colocamos unos Print temporales para que podamos ver lo que estamos escribiendo en la caja de texto y así saber si esta tomando los valores.

--- 04_yes_no/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ 04_yes_no/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -5,6 +5,41 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hola do');
+    //Creamos también un Borde Personalizado para una mejor visualización del Input
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    //?Creamos un decorador de Input Personalizado
+    final inputDecoraction = InputDecoration(
+      //Creamos un HinText para que nuestro input tenga un texto para que se entienda la dinamica.
+      //Este texto esta sobrepuesto
+      hintText: 'Termina tu Mensaje con "?" ',
+      //Colocamos un borde al Input, un color y además lo redondeamos
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      //Creamos un Botón con Icono a la Derecha.
+      suffixIcon: IconButton(
+        onPressed: () {
+          print('Valor de la Caja de Texto?');
+        },
+        icon: const Icon(Icons.send_outlined),
+      ),
+    );
+
+    //!? Creamos un TextFormField donde colocatremos nuestro Input Personalizado
+    return TextFormField(
+      //Le colocamos decoración
+      decoration: inputDecoraction,
+      //Vemos el valor de lo que se esta escribiendo en la caja de texto
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+      },
+      onChanged: (value) {
+        print('changed $value');
+      },
+    );
   }
 }
