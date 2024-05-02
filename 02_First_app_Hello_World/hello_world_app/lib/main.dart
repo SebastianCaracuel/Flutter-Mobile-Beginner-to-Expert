7774aa3f139db7d883a95cd2a2993ceb31cbb134
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-01 23:19:45 -0400

    //

--- 02_First_app_Hello_World/hello_world_app/lib/main.dart
+++ 02_First_app_Hello_World/hello_world_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/Hola_Mundo.dart';
+import 'package:hello_world_app/presentation/screens/hola_mundo.dart';
 
 void main() {
   runApp(const MyApp());
