5d802510448cbcc34ba925e92d578d7119d59bee
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-01 22:43:08 -0400

    Chat Provider
    Creamos nuestro chat provider a nivel global de la aplicación, cada uno de los Widgets hijos tiene acceso de forma directa a poder consultar la información del provider.

--- 04_yes_no/yes_no_app/lib/main.dart
+++ 04_yes_no/yes_no_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [],
+      //Listado de los providers
+      providers: [ChangeNotifierProvider(create: (_) => ChatProvider())],
       child: MaterialApp(
           title: 'Yes No App',
           //Quitamos el banner debug (Aplicación en desarrollo)
