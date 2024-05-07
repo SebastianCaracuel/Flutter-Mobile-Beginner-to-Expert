239cb7daa826f9512603e90685cb8ee0029d279b
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-06 23:26:25 -0400

    Buttons like and Views
    Quitamos los containers con colores, y cambiamos nuestro PageView a PageView.Builder.
    Colocamos un contador con el largo de videos que tenemos en nuestro modelo.
    
    y Retornamos un Widget Stack, ya que tendremos 3 widgets por encima de nuestros videos. El gradiante, los botones y el videoplayer.

--- 05_toktik/toktik_app/lib/presentation/widgets/shared/video_scroll_view.dart
+++ 05_toktik/toktik_app/lib/presentation/widgets/shared/video_scroll_view.dart
@@ -13,19 +13,27 @@ class VideoScrollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //?Es un Widgets que nos permite hacer scroll a pantalla completa.
-    return PageView(
+    return PageView.builder(
       //Creamos un Rebote
       physics: const BouncingScrollPhysics(),
       //Queremos que nuestro Scroll sea vertical
       scrollDirection: Axis.vertical,
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.blue,
-        )
-      ],
+      //
+      itemCount: videos.length,
+      //
+      itemBuilder: (context, index) {
+        //Cargamos nuestro VideoPost
+        final VideoPost videoPost = videos[index];
+
+        //Retornamos un Stack Widget porque tendremos Widgets uno encima de otros
+        return const Stack(
+          children: [
+            //VideoPlayer + gradiente
+
+            //Botones
+          ],
+        );
+      },
     );
   }
 }
