b91c5007f7a989fb9e8672d32a7ec3e38d56721b
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-06 22:56:07 -0400

    Circular Progress
    Creamos una variable que utilizamos para crear una condición, qué sí, no se ha cargado nada, que muestre un Circulo de progreso, y posterior a eso, mostrará los videos.

--- 05_toktik/toktik_app/lib/presentation/screens/discover/discover_screen.dart
+++ 05_toktik/toktik_app/lib/presentation/screens/discover/discover_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik_app/presentation/providers/discover_provider.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Discover Screen")),
-    );
+    //Creamos una variable
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    return Scaffold(
+        //Creamos una condición -
+        body: discoverProvider.initialLoading
+            ? const Center(
+                child: CircularProgressIndicator(
+                //Cambiamos el Grosor de la linea
+                strokeWidth: 2,
+              ))
+            //Aquí irán los videos
+            : const Placeholder());
   }
 }
