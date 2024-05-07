fcb061114ea35ec79b6fd7d276aeb1e4ae192d1f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-07 15:03:44 -0400

    StatefulWidget
    Cambiamos nuestro screen donde se verán los videos de StatelessWidget a Statefulwidget.

--- 05_toktik/toktik_app/lib/presentation/widgets/video/fullscreen_player.dart
+++ 05_toktik/toktik_app/lib/presentation/widgets/video/fullscreen_player.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 //Creamos nuestra pantalla de videos
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   //Colocamos las variables que vamos a utilizar
   final String videoUrl;
   final String caption;
@@ -11,6 +11,11 @@ class FullScreenPlayer extends StatelessWidget {
   const FullScreenPlayer(
       {super.key, required this.videoUrl, required this.caption});
 
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
   @override
   Widget build(BuildContext context) {
     return Placeholder();
