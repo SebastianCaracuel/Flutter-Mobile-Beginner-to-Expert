6e443a4a16c80dd0045393bdf670f0e45638d656
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-12 20:38:56 -0400

    Home Screen Implementation
    Creamos el Home screen y le implementamos nuestro MenuItem. Utilizando el Widget LIstTitle, pudimos llamar a nuestros datos que se encuentra en el menu, también le agregamos el estilo y colores que tenemos en nuestro archivo "app_theme".

--- 06_Widgets_app/w_app/lib/presentation/screens/home/home_screen.dart
+++ 06_Widgets_app/w_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:w_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,64 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+
+      //Cuerpo de nuestro scaffold
+
+      //Importamos nuestro WIdget privado( es privado porque utiliza _ )
+      body: const _HomeView(),
+    );
+  }
+}
+
+//Creamos un widget aparte para la vista de nuestra pantalla o cuerpo. Eso es para no tener un widget tan extenso y nuestro codigo pueda mantenerse en un futuro.
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    //Llamamos a nuestro
+
+    return ListView.builder(
+      //COlocamos un scroll y rebote
+      physics: const BouncingScrollPhysics(),
+      //Este itemcount representa los datos que tenemos en nuestro menu
+      itemCount: appMenuItems.length,
+      //Llamamos a nuestro menu y sus datos.
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTitle(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+//Creamos un Widget personalizado para el contenido en la Lista.
+class _CustomListTitle extends StatelessWidget {
+  const _CustomListTitle({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    //Llamamos a nuestra clase de temas y estilos
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      //Colocamos a nuestros items y le colocamos el color que tenemos asignado
+      leading: Icon(menuItem.icon, color: colors.primary),
+      //Icono que colocaremos al final
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      //Llamamos a nuestro titulo
+      title: Text(menuItem.title),
+      //Llamamos a nuestro subtitulo
+      subtitle: Text(menuItem.subTitle),
+      //Colocamos el OnTap para que se pueda realizar el gesto de "presionar"
+      onTap: () {
+        //todo: Navegar a otra pantalla
+      },
     );
   }
 }
