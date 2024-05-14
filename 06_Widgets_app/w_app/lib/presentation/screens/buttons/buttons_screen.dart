02ea9f6033903d705b81cb624446d9425493945f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-14 13:09:16 -0400

    Custom Button
    Creamos un botón personalizado, y cambiamos los iconos de los demás botones además de sus textos.

--- 06_Widgets_app/w_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ 06_Widgets_app/w_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -50,61 +50,98 @@ class _ButtonsView extends StatelessWidget {
           children: [
             //Creamos nuestro primer botón dentro de nuestro Wrap (Elevated button)
             ElevatedButton(
-                onPressed: () {}, child: const Text('Elevated Button')),
+                onPressed: () {}, child: const Text('Hello People!')),
 
             //Creamos nuestro Segundo botón dentro de nuestro Wrap (Elevated button) - este botón esta desabilitado
             const ElevatedButton(
-                onPressed: null, child: Text('Elevated Disable')),
+                onPressed: null, child: Text('This is a Practicing')),
 
             //Creamos nuestro Tercer botón (Elevated button icon) -
             ElevatedButton.icon(
                 onPressed: () {},
                 icon: const Icon(Icons.image_search_rounded),
-                label: const Text('Elevated Icon')),
+                label: const Text('With the Buttons Widgets')),
 
             //Creamos el Cuarto Botón (FielledButton) -
-            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton(onPressed: () {}, child: const Text('Just')),
 
             //Creamos el Quinto Botón (FielledButton Icons) -
             FilledButton.icon(
                 onPressed: () {},
                 icon: const Icon(Icons.public),
-                label: const Text('data')),
+                label: const Text('Lean!')),
 
             //Creamos el Sexto Botón ( outlineButton)
-            const OutlinedButton(onPressed: null, child: Text('Outline')),
+            const OutlinedButton(onPressed: null, child: Text('A little')),
 
             //Creamos el Septimo Botón ( outlineButton icon)
             OutlinedButton.icon(
                 onPressed: () {},
-                icon: const Icon(Icons.android_rounded),
-                label: const Text('OutlineIcon')),
+                icon: const Icon(Icons.data_object_rounded),
+                label: const Text('bit of')),
 
             //Creamos el Octavo Botón ( TextButton)
-            TextButton(onPressed: () {}, child: const Text('TextButton')),
+            TextButton(onPressed: () {}, child: const Text('From this')),
 
             //Creamos el Noveno Botón ( TextButton icon)
             TextButton.icon(
                 onPressed: () {},
-                label: const Text('TextButton'),
-                icon: const Icon(Icons.terminal_rounded)),
+                label: const Text('Framework'),
+                icon: const Icon(Icons.handyman_rounded)),
 
             //Creamos el Decimo Botón ( IconButton)
             const IconButton(
-                onPressed: null, icon: Icon(Icons.do_not_disturb_alt_outlined)),
+                onPressed: null, icon: Icon(Icons.local_cafe_rounded)),
 
             //Creamos el OnceAvo Botón ( IconButton con style)
             IconButton(
                 onPressed: () {},
-                icon: const Icon(Icons.lock_outline_rounded),
+                icon: const Icon(Icons.flutter_dash_rounded),
                 style: ButtonStyle(
                     //Colocamos el color del botón que sea de nuestra gama de colores
                     backgroundColor: MaterialStatePropertyAll(colors.primary),
                     //Colocamos el color de nuestro icono
-                    iconColor: const MaterialStatePropertyAll(Colors.white)))
+                    iconColor: const MaterialStatePropertyAll(Colors.white))),
 
             //Todo: Custom Button
+            const CustomButton(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+//Creamos nuestro Botón personalizado
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Recibimos el color de nuestra paleta de colores
+    final colors = Theme.of(context).colorScheme;
+
+//utilizamos clipRRect para colocar bordes redondedos
+    return ClipRRect(
+      //Le asignamos el borde
+      borderRadius: BorderRadius.circular(20),
+      //Utilizamos Material para que podamos tener nuestra paleta de colores
+      child: Material(
+        //Le colocamos nuestra variable de paleta de colores
+        color: colors.primary,
+        //Utilizamos un InKWell que es muy parecido a Gesture detector ( Gestor de tacto) - lo que nos permitirá colocar un OnTap.
+        child: InkWell(
+          //Función del presiondo
+          onTap: () {},
+          //Utilizamos un Padding para que tenga espacios y pueda verse bien
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              //Le colocamos un texto
+              child: Text(
+                'Hello Flutter!',
+                //y al texto le colocamo el color blanco para que se pueda ver
+                style: TextStyle(color: Colors.white),
+              )),
         ),
       ),
     );
