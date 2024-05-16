399bc226ec4087b2ccf1916e5bd6d493aed51f0a
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-15 23:04:18 -0400

    Create Screen Snackbars
    Creamos el cascarón de lo que será nuestro Snackbars.

--- 06_Widgets_app/w_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ 06_Widgets_app/w_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -8,8 +8,27 @@ class SnackbarScreen extends StatelessWidget {
   //Constructor
   const SnackbarScreen({super.key});
 
+  //todo: Creamos un metodo que no va a regresar nada
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //Creamos un appbar para poder visualizar el nombre de la pantalla en la que nos encontramos y el button back
+      appBar: AppBar(title: const Text('Snackbars and Dialogs')),
+
+      //Creamos un botón flotante extendido
+      floatingActionButton: FloatingActionButton.extended(
+          //Funcionalidad
+          onPressed: () {
+            //Mostramos un SnackBar con el scaffold
+            ScaffoldMessenger.of(context).showSnackBar(
+                //SnackBar - Mensaje
+                const SnackBar(content: Text('Hello Flutter!')));
+          },
+          //Texto
+          label: const Text('Show SnackBar'),
+          //Icono del botón
+          icon: const Icon(Icons.remove_red_eye_rounded)),
+    );
   }
 }
