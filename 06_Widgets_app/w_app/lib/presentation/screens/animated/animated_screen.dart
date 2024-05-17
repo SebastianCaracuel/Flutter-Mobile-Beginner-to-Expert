65c086f2ff27c2f2b2331e6742bc9044f5f02006
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-17 00:15:20 -0400

    Animated Container
    Transformamos nuestro stateles en un stateful widget y animamos el contenedor.
    Para la animación del contenedor, utilizamos números random, esto es un paquete predeterminado por flutter que se llama math o matematicas. Creamos propiedades de tamaño, propiedad de colores y una propiedad de borderRadius que nos permitirá realizar la animación.

--- 06_Widgets_app/w_app/lib/presentation/screens/animated/animated_screen.dart
+++ 06_Widgets_app/w_app/lib/presentation/screens/animated/animated_screen.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
+//Importamos números random o operaciones matematicas predeterminadas por flutter
+import 'dart:math' show Random; //Crea números aleatorios
+
 //Creamos nuestra Screen
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   //Creamos la variable para el nombre de la pantalla
   static const name = 'animated_screen';
 
   //Constructor
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  //Propiedades para animación
+
+  //Propiedades de tamaño
+  double width = 50;
+  double height = 50;
+  //Propiedad de color
+  Color color = Colors.indigo;
+  //Propiedad de Border Radius
+  double borderRadius = 10.0;
+
+  //Creamos un metodo que no va a regresar nada
+  void changeShape() {
+    //instanciamos nuestro random
+    final random = Random();
+
+    //Colocamos los valores de forma aleatoria
+
+    width = random.nextInt(300) + 120;
+    height = random.nextInt(300) + 120;
+    borderRadius = random.nextInt(100) + 20;
+
+    color = Color.fromRGBO(
+        random.nextInt(255), //blue
+        random.nextInt(255), //red
+        random.nextInt(255),
+        1); //Opacidad
+
+    //Llamamos a nuestor SetState
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,22 +63,24 @@ class AnimatedScreen extends StatelessWidget {
         duration: const Duration(milliseconds: 400),
 
         //Le asginamos el tipo de animación que queremos
-        curve: Curves.easeInOutCubic,
-        //Le colocamos unas medidas a nuestro contenedero
-        width: 100,
-        height: 100,
+        curve: Curves.easeInOutCubicEmphasized,
+
+        //asignamos nuestras propiedades en su  debido lugar
+        width: width <= 0 ? 0 : width,
+        height: height <= 0 ? 0 : height,
         //Decoramos nuestro contenedor
         decoration: BoxDecoration(
             //Le asignamos un color
-            color: Colors.blue,
+            color: color,
             //Le asignamos un borde
-            borderRadius: BorderRadius.circular(20)),
+            borderRadius:
+                BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius)),
       )),
 
       //Botón flotante para la acción de que el container cambie de tamaño
       floatingActionButton: FloatingActionButton(
         //Funcionamiento
-        onPressed: () {},
+        onPressed: changeShape,
         //Icono del botón
         child: const Icon(Icons.play_arrow_rounded),
       ),
