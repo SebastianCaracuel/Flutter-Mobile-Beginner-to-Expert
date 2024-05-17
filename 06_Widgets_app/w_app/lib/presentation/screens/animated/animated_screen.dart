1c14b335c1a94fe63c38c1fd696f637dd2f13e19
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-16 23:51:04 -0400

    Animated container
    Creamos la idea de lo que queremos realizar con este container animado. Dejamos el cascarón del widget para poder implementarlo el la otra clase.

--- 06_Widgets_app/w_app/lib/presentation/screens/animated/animated_screen.dart
+++ 06_Widgets_app/w_app/lib/presentation/screens/animated/animated_screen.dart
@@ -10,6 +10,39 @@ class AnimatedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //Menu
+      appBar: AppBar(
+          //Titulo
+          title: const Text('Animated Container')),
+
+      //Dentro de nuestor body, creamos nuestro container
+      body: Center(
+          //Contenedor
+          child: AnimatedContainer(
+        //Le asignamos un tiempo a la animación
+        duration: const Duration(milliseconds: 400),
+
+        //Le asginamos el tipo de animación que queremos
+        curve: Curves.easeInOutCubic,
+        //Le colocamos unas medidas a nuestro contenedero
+        width: 100,
+        height: 100,
+        //Decoramos nuestro contenedor
+        decoration: BoxDecoration(
+            //Le asignamos un color
+            color: Colors.blue,
+            //Le asignamos un borde
+            borderRadius: BorderRadius.circular(20)),
+      )),
+
+      //Botón flotante para la acción de que el container cambie de tamaño
+      floatingActionButton: FloatingActionButton(
+        //Funcionamiento
+        onPressed: () {},
+        //Icono del botón
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
