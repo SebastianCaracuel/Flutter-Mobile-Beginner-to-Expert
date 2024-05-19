0d3bcc8f63220cab3749a28e44915244af1daf79
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-19 16:09:59 -0400

    Tutorial screen
    Finalizamos el tutorial screen, donde le agregamos una imagen, un titulo y una descripción para que se entienda, al llegar a la ultima pagina se presenta un botón con una leve animación el que nos permite salir o dirigirnos a una nueva screen. En el costado derecho superior se puede ver un botón que nos permite salir del tutorial.
    
    Para esto creamos varias condiciones y propiedades que nos ayudan a la buena interacción entre paginas y botones.

--- 06_Widgets_app/w_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ 06_Widgets_app/w_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,7 +1,30 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+//Creamos una nueva clase //? Esta clase permite realizar slide hacia el costado para hacer navegación
+class SlideInfo {
+  //Propiedades dentro de nuestra nueva clase
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  //Constructor de la clase con las propiedades
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+//Creamos una propiedade para la nueva clase
+final slides = <SlideInfo>[
+  //Slide uno
+  SlideInfo('Selected your food tastes', 'caption', 'assets/images/1.png'),
+  //Slide dos
+  SlideInfo('Delivery will arrive soon', 'caption', 'assets/images/2.png'),
+  //Slide tres
+  SlideInfo('Enojoy!', 'caption', 'assets/images/3.png'),
+];
 
 //Creamos nuestra Screen
-class AppTutorialScreen extends StatelessWidget {
+class AppTutorialScreen extends StatefulWidget {
   //Creamos la variable para el nombre de la pantalla
   static const name = 'tutorial_screen';
 
@@ -9,7 +32,143 @@ class AppTutorialScreen extends StatelessWidget {
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  //Creamos una propiedad para ver en que slide nos encontramos
+  late final PageController pageViewControlle = PageController();
+
+  //Creamos una bandera bool que nos permitira idenfiticar cuando llegamos al final
+  bool endReached = false; // Si no se ha llegado al final
+
+  //ciclo del vida
+  @override
+  void initState() {
+    super.initState();
+    //
+    pageViewControlle.addListener(() {
+      final page = pageViewControlle.page ?? 0;
+
+      //Creamos la condición - la condicion dice que cuando la pagina este por la mitad o apunto de pasar a la final
+      //que se muestre el botón debido a que llegamos al final de las paginas
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        //Llamamos al set
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  //Terminamos con el ciclo de vida del Stateful con el dispose
+  @override
+  void dispose() {
+    pageViewControlle.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //Le agregamos un color de fondo
+      backgroundColor: Colors.white,
+      //Utiizamos un page
+      body: Stack(children: [
+        PageView(
+            //Llamamos al controller para saber en que pagina nos encontramos
+            controller: pageViewControlle,
+            //Agregamos un bouncing o rebote
+            physics: const BouncingScrollPhysics(),
+            children:
+
+                //todo: Creamos nuestro slide
+                slides
+                    .map((slideData) => _Slide(
+                        title: slideData.title,
+                        caption: slideData.caption,
+                        imageUrl: slideData.imageUrl))
+                    .toList()),
+
+        //todo: Colocamos el botón de salir
+        Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+                onPressed: () => context.pop(), child: const Text('Exit!'))),
+
+        //todo: Botón para "iniciar" con condición ya que no queremos que se muestre hasta llegar al final
+        endReached
+            ? Positioned(
+                //Colocamos el botón al final y a la derecha
+                bottom: 30,
+                right: 30,
+                //Agregamos una animación a nuestro botón
+                child: FadeInRight(
+                  //Quiero que ingrese más lento
+                  delay: const Duration(seconds: 1),
+                  //
+                  from: 15,
+                  //Llamamos  a un botón
+                  child: FilledButton(
+                      //La función del botón será salir de la pagina del tutorial
+                      onPressed: () => context.pop(),
+                      //El child es un texto
+                      child: const Text('Continue')),
+                ))
+            : const SizedBox() //y para que funcione nuestra condición agregamos un SizeBox para que no se muestre nuestro botón
+      ]),
+    );
+  }
+}
+
+//Creamos un widget personalizado para nuestro slide
+class _Slide extends StatelessWidget {
+  //Propiedad
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide(
+      {required this.title, required this.caption, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    //Creamos una propiedad dentro de nuestro widget que solo será de este widget
+    final titleStyle = Theme.of(context)
+        .textTheme
+        .titleLarge; //Creamos un titulo de estilo largo
+
+    final captionStyle = Theme.of(context)
+        .textTheme
+        .bodySmall; //Creamos un estilo para nuestra descripción
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //todo: Imagenes del Tutorial
+            Image(image: AssetImage(imageUrl)),
+
+            //Espacio
+            const SizedBox(height: 20),
+
+            //todo:Titulo del tutorial
+            Text(
+              title,
+              style: titleStyle,
+            ),
+            //espacio
+            const SizedBox(height: 10),
+
+            //todo: Descripción (caption)
+            Text(caption, style: captionStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
