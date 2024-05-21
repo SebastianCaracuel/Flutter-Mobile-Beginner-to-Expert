642ba25a577e06e0a5dd36e5285017edb73f10b9
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-21 08:56:30 -0400

    Counter Riverpod Provider

--- 06_Widgets_app/w_app/lib/presentation/providers/counter_provider.dart
+++ 06_Widgets_app/w_app/lib/presentation/providers/counter_provider.dart
@@ -1,5 +1,5 @@
 //Imports
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-//Creamos una variable
+//Creamos una variable o referencia
 final counterProvider = StateProvider((ref) => 5);
