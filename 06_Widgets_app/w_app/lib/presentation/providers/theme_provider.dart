78b6891e425cf6c7b118bfd0cf130e7e0aec500b
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-21 09:52:02 -0400

    Create New Provider Theme
    Creamos un nuevo gestor de estado para manejar nuestra lista de colores gracias al (ColorList) que creamos en Config > theme.
    
    Por lo que creamos una referencia que podamos utilizar para llamar a nuestra lista de colores de forma global y hacer uso de ella.
    Esta referencia es Inmutable por lo que no se podrá cambiar.

--- 06_Widgets_app/w_app/lib/presentation/providers/theme_provider.dart
+++ 06_Widgets_app/w_app/lib/presentation/providers/theme_provider.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:w_app/config/theme/app_theme.dart';
 
-//Creamos un estado para maneajar un bool (Modo oscuro app)
+//?Creamos un estado para maneajar un bool (Modo oscuro app)
+final isDarkModeProvider =
+    StateProvider((ref) => false); //Referencia al modo oscuro
 
-final isDarkModeProvider = StateProvider((ref) => false);
+//?Creamos un estado para manejar nuestra lista de colores(ColorList)
+final colorListProvider = Provider((ref) =>
+    colorList);//Esta referencia es (Inmutable) que yo no lo puedo cambiar.
