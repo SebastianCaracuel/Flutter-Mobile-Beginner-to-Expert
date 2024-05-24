9d316d6c4a35c84200794db220ef8d56424d76f7
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-23 21:07:53 -0400

    Create Repository
    Creamos el repositorio que llamará a nuestro Datasources.

---
+++ 07_Cinemapedia/cinema_app/lib/domain/repositories/movies_repositories.dart
@@ -0,0 +1,16 @@
+//Importaciones de Flutter
+
+//Importaciones nuestras
+import 'package:cinema_app/domain/entities/movie.dart';
+
+//Creamos una clase abstracta //?Es abstracta porque no quiero crear instancias de movie
+
+abstract class MovieRepository {
+  //El repositorio es quien va a llamar a nuestro Datasources
+  //Definimos como lucen los origines de datos que puede traer la API
+
+  //?Traeme las peliculas que están en cartelera -
+  //?La información de retorno va a ser un Future que regresa una lista de la entidad
+  Future<List<Movie>> getNoPlaying(
+      {int page = 1}); //Necesito que me especifique la pagina
+}
