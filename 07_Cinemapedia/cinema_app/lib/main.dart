b15c98d5fe9e8fbc18f5cc600b5d3acd93032a7c
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-24 21:03:17 -0400

    Implementation Riverpod
    Implementamos el ProviderScope en nuestro main, ya que tendrá la referencia de todos nuestros providers.

--- 07_Cinemapedia/cinema_app/lib/main.dart
+++ 07_Cinemapedia/cinema_app/lib/main.dart
@@ -4,13 +4,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 //Importaciones nuestras
 import 'package:cinema_app/config/router/app_router.dart';
 import 'package:cinema_app/config/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   //Definimos el paquete .env
   await dotenv.load(fileName: '.env');
 
   //Corremos la aplicación
-  runApp(const MainApp());
+  runApp(
+      //Implementamos Riverpod - (ProviderScope) que tiene la referencia de todos nuestros providers
+      const ProviderScope(
+          //llamamos a la aplicación
+          child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
