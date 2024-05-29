6e50b468298dbefc99d56acf3eb7ad800174c20a
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-29 14:50:11 -0400

    InfiniteScroll horizontal
    Evitamos las peticiones simultaneas en nuestro cargado de páginas utilizando una bandera bool, también colocamos un await para que las páginas se renderizen correctamente.

--- 07_Cinemapedia/cinema_app/lib/presentation/providers/movies/movies_providers.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/providers/movies/movies_providers.dart
@@ -11,6 +11,10 @@ typedef MovieCallBack = Future<List<Movie>> Function({int page});
 //Creamos una clase que nos permita a nostros la reutilización
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   //Propiedades de la clase
+
+  //?Creamos una bandera para evitar las pecticiones simultáneas de películas
+  bool isLoading = false;
+
   int currentPage = 0; //Conocer la pagina actual
   //Llamamos la función que creamos "Typedef"
   MovieCallBack fetchMoreMovies;
@@ -22,6 +26,10 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   //Creamos un metodo
   Future<void> laodNextPage() async {
+    //?Cuando cargue el loadNextPage preguntaremos sí, esta cargando no hacemos nada, pero si no esta cargando
+    if (isLoading) return;
+    isLoading = true;
+
     //El Objetivo de este metodo es hacerle una modificación al State ( El listado de movie)
     currentPage++; //Incrementamos en uno nuestro currentPage (Pagina actual)
 
@@ -30,6 +38,12 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
 
     //Regresamos un nuevo State - Mandamos todas las "Películas que nosotros queremos agregar"
     state = [...state, ...movies];
+
+    //Como las películas cargan muy rapido, por lo que queremos que las películas sean renderizadas
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    //Cuando ya tenemos cargado el estado, colocamos la variable en falso
+    isLoading = false;
   }
 }
 
