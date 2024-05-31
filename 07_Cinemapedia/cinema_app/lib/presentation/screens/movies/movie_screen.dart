3d5195bbecdfc04d004b647e01f4f7f1bc9daf35
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-05-30 21:40:17 -0400

    Create movie_screen.dart
    Creamos la nueva pantalla de película, donde le asignamos un nombre "movie-screen" para poder hacer la navegación respectiva con Go_Router.
    También definimos una propiedad "movieID" que nos ayudará a entender un poco mejor lo de DeepLinkin.
    Por ahora, solo creamos el cascarón de esta nueva página, que nos permitirá navegar a una película individualmente y obtener la información que esta tiene.

---
+++ 07_Cinemapedia/cinema_app/lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,27 @@
+//Importaciones Flutter
+import 'package:flutter/material.dart';
+
+//Importaciones nuestras
+
+//Clase
+class MovieScreen extends StatelessWidget {
+  //Propiedades
+
+  //Nombre de la pantalla
+  static const name = 'movie-screen';
+
+  //?Definimos como propiedad el Id de una película.
+  final String movieId;
+
+  //Constructor
+  const MovieScreen({super.key, required this.movieId});
+
+  //Objeto
+  @override
+  Widget build(BuildContext context) {
+    //Propiedades objeto
+
+    //Widget padre
+    return Container();
+  }
+}
