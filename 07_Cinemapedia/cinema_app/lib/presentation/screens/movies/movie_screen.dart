94a280d8ac1507225af973a52b9f2692ca252413
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-01 01:14:33 -0400

    HTTP Request and Test Caché
    Cambiamos nuestra movie screen de un statefulWidget a un ConsumerStateFulWidget.
    
    Llamamos a nuestra referencia del provider de Movie Info y obtener las películas con el ID especificado, creamos una condición si la película no está cargada(es nula), mostramos un indicador de carga, si la película está cargada, mostramos la interfaz de la pantalla de la película.

--- 07_Cinemapedia/cinema_app/lib/presentation/screens/movies/movie_screen.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/screens/movies/movie_screen.dart
@@ -1,10 +1,13 @@
 //Importaciones Flutter
+import 'package:cinema_app/domain/entities/movie.dart';
+import 'package:cinema_app/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //Importaciones nuestras
 
 //Clase
-class MovieScreen extends StatefulWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   //Propiedades
 
   //Nombre de la pantalla
@@ -17,33 +20,36 @@ class MovieScreen extends StatefulWidget {
   const MovieScreen({super.key, required this.movieId});
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
 //State
-class _MovieScreenState extends State<MovieScreen> {
+class MovieScreenState extends ConsumerState<MovieScreen> {
   //State propiedades
 
-//Cuando estoy cargando
+// Se llama al initsate cuando el estado se inicializa
   @override
   void initState() {
     super.initState();
 
-    //
-  }
-
-//Cuando termine de cargar
-  @override
-  void dispose() {
-    //
-
-    super.dispose();
+    // Usamos `ref` para leer el proveedor `movieInfoProvider` y cargar la película usando su ID
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
   }
 
   //Objeto
   @override
   Widget build(BuildContext context) {
     //Propiedades objeto
+
+    // Utilizamos `ref` para observar el estado del proveedor `movieInfoProvider` y obtener la película con el ID especificado
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    // Si la película no está cargada (es nula), mostramos un indicador de carga
+    if (movie == null) {
+      return const Scaffold(
+          body: Scaffold(
+              body: Center(child: CircularProgressIndicator(strokeWidth: 4))));
+    }
 
     //Widget padre
     return Scaffold(
