68e459d73ccbfa27c259ed31118756a48f0ffd12
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-03 21:38:00 -0400

    Desing Movie Screen
    Creamos el diseño para que podamos ver la película que escogemos, creamos un appBar personalizado, donde le agregamos la imagen de la película y dos gradientes.
    Esto es solo el cascarón del diseño para que podamos ver la película, en la próxima etapa agregaremos la descripción de la película.

--- 07_Cinemapedia/cinema_app/lib/presentation/screens/movies/movie_screen.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/screens/movies/movie_screen.dart
@@ -53,8 +53,108 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
 
     //Widget padre
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('MovieID: ${widget.movieId}')),
+      //
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        //
+        slivers: [_CustomSliverAppbar(movie: movie)],
+      ),
+    );
+  }
+}
+
+//
+class _CustomSliverAppbar extends StatelessWidget {
+  //Propiedades
+
+  //Creamos una variable que llame a nuestra película
+  final Movie movie;
+
+  //Constructor
+  const _CustomSliverAppbar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    //Propiedades del Objeto
+
+    //?Creamos una variable que nos trae las dimensiones del dispositivo fisicamente
+    final size = MediaQuery.of(context).size;
+
+    //Creamos un appbar Personalizado o una barra superior
+    return SliverAppBar(
+      //Le agregamos un color a nuestro fondo
+      backgroundColor: Colors.black,
+      //Utilizamos la variable de las dimensiones porque quiero el 70 porciento de la pantalla
+      expandedHeight: size.height * 0.7,
+      //Utilizamos foreground para los objetos que esten dentro de nuestro appbar personalizado, que se coloque en blanco
+      foregroundColor: Colors.white,
+      //Este es el espacio flexible de nuestro custom appbar
+      flexibleSpace: FlexibleSpaceBar(
+        //Ahora queremos tener Objetos dentro de nuestro Appbar
+        background: Stack(
+          //Para esto utilizamos un Stack - para colocar Widgets encima de otros
+          children: [
+            //Utilizamos un sizebox expandido para que pueda verse completa nuestra imagen
+            SizedBox.expand(
+                //Utilizamos image.network para llamar a la imagen de nuestra película
+                child: Image.network(
+              //Llamamos a las variables para mostrar la imagen
+              movie.posterPath,
+              //Colocamos un fit.cover para que la imagen se expanda y se vea completo
+              fit: BoxFit.cover,
+            )),
+
+            //Creamos un gradiante para poder ver el cuerpo de la imagen y los objetos en su interior
+            const SizedBox.expand(
+                //Utilizamos una decoración
+                child: DecoratedBox(
+              //un Boxdecoration
+              decoration: BoxDecoration(
+                //Colocamos un gradiente
+                gradient: LinearGradient(
+                  //Donde comienza el gradiente
+                  begin: Alignment.topCenter,
+                  //Donde termina el gradiente
+                  end: Alignment.bottomCenter,
+                  //COnfiguramos los stops
+                  stops: [0.7, 1.0],
+                  //Utilizamos dos colores siempre en nuestro gradiente
+                  colors: [
+                    //un color claro o transparente
+                    Colors.transparent,
+                    //Y el color que simule la sombra
+                    Colors.black87
+                  ],
+                ),
+              ),
+            )),
+
+            //Creamos un nuevo gradiente para poder ver el botón superior
+            const SizedBox.expand(
+                //Utilizamos una decoración
+                child: DecoratedBox(
+              //un Boxdecoration
+              decoration: BoxDecoration(
+                //Colocamos un gradiente
+                gradient: LinearGradient(
+                  //Donde comienza el gradiente
+                  begin: Alignment.topLeft,
+                  //COnfiguramos los stops
+                  stops: [0.0, 0.4],
+                  //Utilizamos dos colores siempre en nuestro gradiente
+                  colors: [
+                    //Y el color que simule la sombra
+                    Colors.black87,
+
+                    //un color claro o transparente
+
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+            )),
+          ],
+        ),
       ),
     );
   }
