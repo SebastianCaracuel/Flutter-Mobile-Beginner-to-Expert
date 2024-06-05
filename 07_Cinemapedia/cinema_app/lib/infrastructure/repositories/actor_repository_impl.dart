2516a25b2aab70aeed60903750a35d07e3ebc237
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-04 20:10:06 -0400

    Create Repository Impl
    Creamos una clase para el repositorio implementación que llamará a nuestro datasource.

---
+++ 07_Cinemapedia/cinema_app/lib/infrastructure/repositories/actor_repository_impl.dart
@@ -0,0 +1,26 @@
+//Importaciones flutter
+
+//Importaciones Nuestras
+import 'package:cinema_app/domain/datasources/actors_datasources.dart';
+import 'package:cinema_app/domain/entities/actor.dart';
+import 'package:cinema_app/domain/repositories/actors_repositories.dart';
+
+//Creamos una clase para nuestro repositorio que llamara al datasource
+
+class ActorRepositoryImpl extends ActorsRepository {
+  //Propiedades de la clase
+
+  //?Llamamos al datasource del actor
+  final ActorsDatasource datasource;
+
+  //Constructor de la clase
+  ActorRepositoryImpl({required this.datasource});
+
+  @override
+  Future<List<Actor>> getActorsByMovie(String movieId) {
+    //Propiedades del metodo obtener actores
+
+    //Implementamos el datasources y el metodo para obtener los datos
+    return datasource.getActorsByMovie(movieId);
+  }
+}
