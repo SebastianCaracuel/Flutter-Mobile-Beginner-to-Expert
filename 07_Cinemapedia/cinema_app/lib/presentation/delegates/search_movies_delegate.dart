0399ac8cf84f076b1abeeb63439b0d13c3ba7e14
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-05 20:03:35 -0400

    SearchDelegate - Leading and Actions
    Añadimos dos botones nuevos en nuestro SearchDelegate, uno de ellos es para volver a la pantalla principal, un back arrow y el otro es para limpiar lo que anoto el usuario en el "search". Este "limpiador" tiene una animación y solo aparece cuando hay datos en el "Search".

--- 07_Cinemapedia/cinema_app/lib/presentation/delegates/search_movies_delegate.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/delegates/search_movies_delegate.dart
@@ -1,10 +1,12 @@
 //Importaciones flutter
+
 import 'package:flutter/material.dart';
 //Importaciones nuestras
+import 'package:cinema_app/domain/entities/movie.dart';
+import 'package:animate_do/animate_do.dart';
 
 //Creamos una clase
-
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   // Esta propiedad sobreescribe el texto del marcador de posición (placeholder)
   // del campo de búsqueda. Cuando el usuario ve el campo de búsqueda vacío,
   // verá "search movie" en lugar del texto por defecto (search). Esto ayuda a indicar
@@ -16,14 +18,35 @@ class SearchMovieDelegate extends SearchDelegate {
   // Normalmente, incluiría widgets como un botón para limpiar la búsqueda.
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [const Text('buildActions')];
+    return [
+      // El widget FadeIn anima su hijo (child) cuando su propiedad `animate` es verdadera.
+      FadeIn(
+        // La animación solo se activa si la consulta (query) no está vacía.
+        animate: query.isNotEmpty,
+        // Duración de la animación (200 milisegundos).
+        duration: const Duration(milliseconds: 200),
+        // El hijo del FadeIn es un IconButton.
+        child: IconButton(
+          // Cuando se presiona el botón, la consulta se borra (se establece en una cadena vacía).
+          onPressed: () => query = '',
+          // El icono mostrado en el botón es un ícono de limpiar (clear).
+          icon: const Icon(Icons.clear),
+        ),
+      ),
+    ];
   }
 
   // Este método construye el widget que aparece al principio del AppBar,
   // como un botón de retroceso para cerrar la búsqueda.
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      // Utilizamos un Onpressed para ir "hacia atras" la pagina principal
+      // En este caso, se llama a `close` para cerrar la búsqueda y se pasa `null` como resultado.
+      onPressed: () => close(context, null),
+      // El icono mostrado en el botón es una flecha hacia atrás.
+      icon: const Icon(Icons.arrow_back),
+    );
   }
 
   // Este método construye los resultados de la búsqueda basados en la consulta del usuario.
