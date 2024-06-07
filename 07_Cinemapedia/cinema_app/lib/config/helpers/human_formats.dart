b58a4c508d9bf65aea5c8c8b65112566556d75d0
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-06 21:30:53 -0400

    Upgrade Human Formats
    Agregamos en el método estático llamado number que toma un número de tipo double la opcionalmente de un entero que representa la cantidad de decimales (por defecto es 0).

--- 07_Cinemapedia/cinema_app/lib/config/helpers/human_formats.dart
+++ 07_Cinemapedia/cinema_app/lib/config/helpers/human_formats.dart
@@ -5,14 +5,15 @@ import 'package:intl/intl.dart';
 
 //Creamos una clase que nos permita visualizar formatos númericos - humanamente posibles
 class HumanFormats {
-  //Metodo
-  static String number(double number) {
+  // Definimos un método estático llamado number que toma un número de tipo double
+  // y opcionalmente un entero que representa la cantidad de decimales (por defecto es 0)
+  static String number(double number, [int decimals = 0]) {
     //Llamamos al paquete Intl
     final formattedNumber = NumberFormat.compactCurrency(
       //Realizamos la configuración de compo queremos el formato
 
       //?Numeros decimales en 0, porque no queremos
-      decimalDigits: 0,
+      decimalDigits: decimals,
       //?Que no tenga simbolos
       symbol: '',
       //?El idioma local - En este caso utilizarémos ingles.
