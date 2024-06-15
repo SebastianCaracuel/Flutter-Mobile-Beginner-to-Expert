cce822075c2f20571585288f86fa656039e71c54
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-15 18:44:59 -0400

    Upgrade Go_Router
    Actualizamos la configuración del Go_Router también.

--- 07_Cinemapedia/cinema_app/lib/config/router/app_router.dart
+++ 07_Cinemapedia/cinema_app/lib/config/router/app_router.dart
@@ -1,52 +1,34 @@
 //Importación de Flutter
 import 'package:go_router/go_router.dart';
 //Importaciones nuestras
-import 'package:cinema_app/presentation/views/views.dart';
 import 'package:cinema_app/presentation/screens/all_screens.dart';
 
 //Creamos la clase implementando el Go_Router
 final appRouter = GoRouter(
   //?Creamos una ruta inicial
-  initialLocation: '/',
+  initialLocation: '/home/0',
 
   routes: [
-    //Todo: Utilización del ShellRoute
-    ShellRoute(
-      //Es algo que se va a llamar en tiempo de ejecución para construir algo
-      builder: (context, state, child) {
-        //Llamamos al HomeScreen para que tenga el Appbar,
-        //Pero llamamos también a la vista del HomeScreen
-        return HomeScreen(childView: child);
+    GoRoute(
+      //Home Vista
+      path: '/home/:page',
+      builder: (context, state) {
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+        return HomeScreen(pageIndex: pageIndex);
       },
-      //Las rutas
+      //Las routes Hijas
       routes: [
         GoRoute(
-          //Home Vista
-          path: '/',
-          builder: (context, state) {
-            return const HomeView();
-          },
-          //Las routes Hijas
-          routes: [
-            GoRoute(
-                //Ruta definida para la pantalla
-                path: 'movie/:id',
-                //Nombre definido para la pantalla
-                name: MovieScreen.name,
-                //Builder
-                builder: (context, state) {
-                  //?Como obtengo el ID de la película
-                  final movieId = state.pathParameters['id'] ?? 'no-id';
-
-                  return MovieScreen(movieId: movieId);
-                }),
-          ],
-        ),
-        GoRoute(
-            // Favoritos vista
-            path: '/favorites',
+            //Ruta definida para la pantalla
+            path: 'movie/:id',
+            //Nombre definido para la pantalla
+            name: MovieScreen.name,
+            //Builder
             builder: (context, state) {
-              return const FavoritesViews();
+              //?Como obtengo el ID de la película
+              final movieId = state.pathParameters['id'] ?? 'no-id';
+
+              return MovieScreen(movieId: movieId);
             }),
       ],
     ),
