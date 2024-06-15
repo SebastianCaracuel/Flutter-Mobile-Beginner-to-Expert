663fb58aa72bc689d85d63c3dbe95f42fa4342aa
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-15 12:33:11 -0400

    Upgrade Custom Buttom Navigation Bar
    Creamos un método de navegación dentro de nuestra barra de navegación.
    Donde utilizamos un Switch como condición para navegar, en caso de presionar el primer icono, navegamos al home y así con los demás iconos.

--- 07_Cinemapedia/cinema_app/lib/presentation/widgets/shared/custom_buttombar.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/widgets/shared/custom_buttombar.dart
@@ -1,10 +1,38 @@
 //IMportaciones flutter
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 //IMportaciones nuestras
 
 //Creamos el widget
 class CustomButtomBar extends StatelessWidget {
   //Propiedades de la clase
+
+  //?Creamos un método
+  void onItemTapped(BuildContext context, int index) {
+    //Utilizamos un Switch basado en el Index
+    switch (index) {
+      //Cuando se hace click en la primera opción
+      case 0:
+        //Va a navegar al Home
+        context.go('/');
+        //Se utiliza el Break para que luego de la navegación no haga nada más
+        break;
+
+      //Cuando se hace click en la primera opción
+      case 1:
+        //Va a navegar a las categorias
+        context.go('/');
+        //Se utiliza el Break para que luego de la navegación no haga nada más
+        break;
+
+      //Cuando se hace click en la primera opción
+      case 2:
+        //Va a navegar a los Favoritos
+        context.go('/favorites');
+        //Se utiliza el Break para que luego de la navegación no haga nada más
+        break;
+    }
+  }
 
   //Constructor
   const CustomButtomBar({super.key});
@@ -18,6 +46,8 @@ class CustomButtomBar extends StatelessWidget {
     return BottomNavigationBar(
       //Le quitamos la línea que se ve en la barra se llama "elevación"
       elevation: 0,
+      //Colocamos un onTap para poder llamar a nuestro método de navegación
+      onTap: (index) => onItemTapped(context, index),
       //Tenemos que colocar los items que tendrá nuestra barra de navegación inferior
       items: const [
         //todo: Creamos el primer item - home
