67fce173690df3df896eb2fdcbe4d2fd4f0300da
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-16 11:57:46 -0400

    Implement Favorite Movies Provider
    Implementamos el Provider de películas favoritas, para que en nuestra vista de "Favoritos" podamos ver el nombre de las películas que nosotros escogimos como favoritas.

--- 07_Cinemapedia/cinema_app/lib/presentation/views/Movies/favorite_views/favorites_views.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/views/Movies/favorite_views/favorites_views.dart
@@ -1,23 +1,54 @@
 //Importaciones flutter
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 //Importaciones Nuestras
+import 'package:cinema_app/presentation/providers/providers.dart';
 
-//Clase para la vista de las vistas de Favoritos
-class FavoritesViews extends StatelessWidget {
+//Clase para la vista de las películas Favoritas - Cambiamos de un Stateless a un ConsumerStatefulWidget
+class FavoritesViews extends ConsumerStatefulWidget {
   //Propiedades de la vista
 
   //Constructor
   const FavoritesViews({super.key});
 
   @override
+  FavoritesViewsState createState() => FavoritesViewsState();
+}
+
+//ConsumerStateFulWidget
+class FavoritesViewsState extends ConsumerState<FavoritesViews> {
+  //Inicia en
+  @override
+  void initState() {
+    super.initState();
+
+    //Llamamos a nuestra referencia del Provider de las películas Favoritas
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  //Objeto
+  @override
   Widget build(BuildContext context) {
+    //Propiedades del Objeto
+
+    //Le colocamos una variable que podamos utilizar a nuestro provider de películas Favoritas
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
-      ),
-      body: const Center(
-        child: Text('Favorites View'),
-      ),
-    );
+        body: ListView.builder(
+      //Llamamos al largo de nuestras películas Favoritas
+      itemCount: favoriteMovies.length,
+      itemBuilder: (context, index) {
+        //Propiedades del Builder
+
+        //?Colocamos una variable que podamos utilizar a nuestro provider con el indice
+        final movie = favoriteMovies[index];
+
+        //Widget Padre
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    ));
   }
 }
