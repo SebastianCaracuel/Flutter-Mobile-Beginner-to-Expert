b9069bb1e746238cc51b7c8237e85374e1e0512c
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-15 23:06:38 -0400

    Isar Datasource Implementation
    Implementamos el Datasource de Isar.

--- 07_Cinemapedia/cinema_app/lib/infrastructure/datasources/isar_datasource.dart
+++ 07_Cinemapedia/cinema_app/lib/infrastructure/datasources/isar_datasource.dart
@@ -1,5 +1,6 @@
 //Importaciones Flutter
-
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 //Importaciones Nuestas
 
 import 'package:cinema_app/domain/datasources/local_storage_datasource.dart';
@@ -7,6 +8,27 @@ import 'package:cinema_app/domain/entities/movie.dart';
 
 //
 class IsarDatasoruce extends LocalStorageDatasource {
+  // Declaramos una variable tardía que contendrá el Future de la base de datos Isar
+  late Future<Isar> db;
+
+  // Constructor que inicializa la variable db llamando al método openDB
+  IsarDatasoruce() {
+    db = openDB();
+  }
+  // Método que abre la base de datos Isar y retorna un Future de Isar
+  Future<Isar> openDB() async {
+    // Obtiene el directorio de documentos de la aplicación
+    final dir = await getApplicationDocumentsDirectory();
+
+    // Si no hay instancias de Isar abiertas, abre una nueva con el esquema de Movie y el inspector habilitado
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema],
+          inspector: true, directory: dir.path);
+    }
+    // Si ya existe una instancia, la devuelve
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMovieFavorite(int movieId) {
     // TODO: implement isMovieFavorite
@@ -14,14 +36,14 @@ class IsarDatasoruce extends LocalStorageDatasource {
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
     throw UnimplementedError();
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
     throw UnimplementedError();
   }
 }
