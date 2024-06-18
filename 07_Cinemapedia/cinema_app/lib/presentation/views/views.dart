87584ba3ce30191ea3051dcb7517348ed20a3174
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-18 00:06:51 -0400

    Export View Barril File
    Exportamos nuestra nueva vista al barril de vistas.

--- 07_Cinemapedia/cinema_app/lib/presentation/views/views.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/views/views.dart
@@ -1,3 +1,4 @@
 //Este es el archivo de barril para las vistas
 export 'Movies/favorite_views/favorites_views.dart';
 export 'Movies/home_views/home_view.dart';
+export 'Movies/popular_views/popular_view.dart';
