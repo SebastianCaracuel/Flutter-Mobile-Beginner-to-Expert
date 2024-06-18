721d042eb3be66a212e9d39c1e6b54c92def4144
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-17 22:32:54 -0400

    Create barril file models
    Creamos el archivo de barril de los modelos y exportamos allí las rutas, para luego ser llamadas de una forma más optimizada.

---
+++ 07_Cinemapedia/cinema_app/lib/infrastructure/models/models.dart
@@ -0,0 +1,6 @@
+//Este es un archivo de barril para los modelos
+export 'moviedb/credits_response.dart';
+export 'moviedb/movie_details.dart';
+export 'moviedb/movie_moviedb.dart';
+export 'moviedb/moviedb_response.dart';
+export 'moviedb/moviedb_videos.dart';
