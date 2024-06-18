606deffa928d5482ef237579a8df24e87c578f19
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-18 01:23:34 -0400

    Update widgets.dart
    Actualizamos el archivo de barril de widgets exportando el nuevo widget, "similar_movies.dart".

--- 07_Cinemapedia/cinema_app/lib/presentation/widgets/widgets.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/widgets/widgets.dart
@@ -5,6 +5,7 @@ export 'movies/movie_masonrygrid.dart';
 export 'movies/movie_poster_link.dart';
 export 'movies/movie_rating.dart';
 export 'movies/movies_slideshow.dart';
+export 'movies/similar_movies.dart';
 export 'shared/custom_appbar.dart';
 export 'shared/custom_buttombar.dart';
 export 'shared/FullScreen_loader.dart';
