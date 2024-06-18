1860368169a4b5fc51e390757ad6eacd51f30016
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-18 01:05:10 -0400

    Update widget movie_poster_link.dart
    Actualizamos el Widget, con imagen de cargas, animaciones y retrasos para que las animaciones comiencen.

--- 07_Cinemapedia/cinema_app/lib/presentation/widgets/movies/movie_poster_link.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/widgets/movies/movie_poster_link.dart
@@ -1,9 +1,11 @@
 //Importaciones Flutter
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:animate_do/animate_do.dart';
 //Importaciones nuestras
-import 'package:cinema_app/domain/entities/movie.dart';
-import 'package:go_router/go_router.dart';
+import 'package:cinema_app/domain/entities/entities.dart';
 
 //? Este Widget trata sobre
 
@@ -22,18 +24,37 @@ class MoviePosterLink extends StatelessWidget {
   Widget build(BuildContext context) {
     //Propiedades del Objeto
 
+    //?
+    final random = Random();
+
     //Widget Padre | Utilizamos un Gestor de detecciónes para navegar a la película Favorita
     //Utilizamos un FadeInUp Para una animación, utilizando animate do de fernando herrero
     return FadeInUp(
+      // 'from' especifica la distancia vertical desde la que el widget comenzará a moverse hacia arriba
+      // random.nextInt(100) genera un número aleatorio entre 0 y 99
+      // Al sumarle 80, el rango final es de 80 a 179, creando variabilidad en la posición inicial de la animación
+      from: random.nextInt(100) + 80,
+
+      // 'delay' especifica el retraso antes de que comience la animación
+      // random.nextInt(450) genera un número aleatorio entre 0 y 449 milisegundos
+      // Esto introduce un retraso aleatorio antes de que la animación comience, haciendo que cada instancia se inicie en un momento diferente
+      delay: Duration(milliseconds: random.nextInt(450) + 0),
       child: GestureDetector(
         //? Al presionar la tarjeta, nos redireccionará a la película seleccionada
         onTap: () => context.push('/home/0/movie/${movie.id}'),
         // Utilizamos un ClipRRect para que tenga bordes redondendados
         child: ClipRRect(
-          //Colocamos bordes redondeados a nuestras tarjetas
-          borderRadius: BorderRadius.circular(20),
-          child: Image.network(movie.posterPath),
-        ),
+            //Colocamos bordes redondeados a nuestras tarjetas
+            borderRadius: BorderRadius.circular(20),
+            //Utilizamos un FadeInImage para una carga de imagen
+            child: FadeInImage(
+              //Le damos un alto a la imagen
+              height: 180,
+              //Que la imagen ocupe todo el espaico y se expanda
+              fit: BoxFit.cover,
+              placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+              image: NetworkImage(movie.posterPath),
+            )),
       ),
     );
   }
