2d05de2e05355dc0161e88a1ddc38e9075b67c05
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-18 13:37:46 -0400

    property Dark Mode
    Creamos la propiedad Dark Mode para luego implementarla en nuestra aplicación.

--- 07_Cinemapedia/cinema_app/lib/config/theme/app_theme.dart
+++ 07_Cinemapedia/cinema_app/lib/config/theme/app_theme.dart
@@ -3,10 +3,25 @@ import 'package:flutter/material.dart';
 
 //Creamos una clase para nuestro tema
 class ApptTheme {
+  //Propiedades
+
+  //? Cambiar de color a darkmode
+  final bool isDarkMode;
+
+  //COnstructor
+  ApptTheme({this.isDarkMode = false});
+
   ThemeData getTheme() => ThemeData(
         //Material 3
         useMaterial3: true,
         //Color de la aplicación
         colorSchemeSeed: const Color(0xff2862F5),
+        //todo:Darkmode
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
+      );
+
+  //Creamos un método que es la copia de la clase
+  ApptTheme copyWith({bool? isDarkmode}) => ApptTheme(
+        isDarkMode: isDarkmode ?? isDarkMode,
       );
 }
