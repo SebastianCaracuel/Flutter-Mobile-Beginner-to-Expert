87f3bd088866cbf632dce77e5630f15be07b8904
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-17 22:44:01 -0400

    Update human_formats.dart
    Creamos una nueva función estática y creamos un formato de fecha, año, mes y día de la semana.

--- 07_Cinemapedia/cinema_app/lib/config/helpers/human_formats.dart
+++ 07_Cinemapedia/cinema_app/lib/config/helpers/human_formats.dart
@@ -9,7 +9,7 @@ class HumanFormats {
   // y opcionalmente un entero que representa la cantidad de decimales (por defecto es 0)
   static String number(double number, [int decimals = 0]) {
     //Llamamos al paquete Intl
-    final formattedNumber = NumberFormat.compactCurrency(
+    return NumberFormat.compactCurrency(
       //Realizamos la configuración de compo queremos el formato
 
       //?Numeros decimales en 0, porque no queremos
@@ -19,8 +19,16 @@ class HumanFormats {
       //?El idioma local - En este caso utilizarémos ingles.
       locale: 'en',
     ).format(number);
+  }
 
-    //Retornamos
-    return formattedNumber;
+  // Define una función estática llamada 'shortDate' que toma un objeto 'DateTime' como parámetro
+  static String shortDate(DateTime date) {
+    // Crea un formato de fecha específico utilizando la clase 'DateFormat'
+    // 'yMMMEd' significa: año, mes abreviado y día de la semana
+    // 'es' indica que el formato de fecha será en español
+    final format = DateFormat.yMMMEd('es');
+
+    // Formatea la fecha proporcionada según el formato especificado y la devuelve como una cadena
+    return format.format(date);
   }
 }
