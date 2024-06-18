2eaf447f2b87b63fba00816d3efa9f6bd2982b02
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-18 00:50:21 -0400

    Update widgets.dart
    Actualizamos el archivo de barril Widget, exportando el nuevo Widget Movie Rating.

--- 07_Cinemapedia/cinema_app/lib/presentation/widgets/widgets.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/widgets/widgets.dart
@@ -3,6 +3,7 @@ export 'actors/actors_by_movie.dart';
 export 'movies/movie_horizontal_listview.dart';
 export 'movies/movie_masonrygrid.dart';
 export 'movies/movie_poster_link.dart';
+export 'movies/movie_rating.dart';
 export 'movies/movies_slideshow.dart';
 export 'shared/custom_appbar.dart';
 export 'shared/custom_buttombar.dart';
