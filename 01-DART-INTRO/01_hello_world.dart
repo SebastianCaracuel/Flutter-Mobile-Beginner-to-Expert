7aa88f99cb9dd167ac713c3a1a72d88d4bb33e8d
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-18 23:18:02 -0400

    Create Hello_world.dart
    Creamos el primer Hola mundo en el lenguaje Dart.

---
+++ 01-DART-INTRO/01_hello_world.dart
@@ -0,0 +1,15 @@
+void main(){
+  
+  // var myName = "Sebastián";
+  // late final myName;
+  // String myName = 'Sebastián';
+  
+  const myName = 'Sebastián';
+  const helloWorld = "Hola Mundo";
+  
+  print('Hola $myName');
+  print( '${myName.toUpperCase() }' ); 
+  print( '${helloWorld.toLowerCase() }' ); 
+  print ('¿Cuanto es 1 + 1? : ${ 1 + 1 }');
+  
+}
