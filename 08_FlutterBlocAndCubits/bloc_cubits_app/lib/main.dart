45785f6a0c330fd57643d5f32ae35cc9235b085a
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-20 21:58:26 -0400

    Create AppTheme
    Creamos el tema de la aplicación, y la implementamos en nuestro Main.dart

--- 08_FlutterBlocAndCubits/bloc_cubits_app/lib/main.dart
+++ 08_FlutterBlocAndCubits/bloc_cubits_app/lib/main.dart
@@ -1,16 +1,32 @@
+//Importaciones Flutter
 import 'package:flutter/material.dart';
 
+//Importaciones nuestras
+import 'package:bloc_cubits_app/config/theme/app_theme.dart';
+
+//Devolvemos nuestro Widget
 void main() {
   runApp(const MainApp());
 }
 
+//Creamos la clase main para que corra nuestra aplicación
 class MainApp extends StatelessWidget {
+  //Propiedades
+
+  //Constructor
   const MainApp({super.key});
 
+  //Objeto
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    //Propieades del objeto
+
+    //Widget Padre
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false, //? Le quitamos el Debug
+      //Colocamos nuestro tema de la aplicación
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
