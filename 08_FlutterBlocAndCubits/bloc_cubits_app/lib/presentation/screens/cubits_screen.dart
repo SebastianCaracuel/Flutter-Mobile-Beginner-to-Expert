48bf420db67fa56c02eba6df7d43e67d0f56aa9a
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-06-20 23:00:37 -0400

    CubitsScreen.dart
    Creamos el cascarón de Cubits Screen.

--- 08_FlutterBlocAndCubits/bloc_cubits_app/lib/presentation/screens/cubits_screen.dart
+++ 08_FlutterBlocAndCubits/bloc_cubits_app/lib/presentation/screens/cubits_screen.dart
@@ -21,9 +21,71 @@ class CubitsScreen extends StatelessWidget {
       //Colocamos una barra superior
       appBar: AppBar(
         title: const Center(child: Text('Cubits Counter')),
+        actions: [
+          //Coloamos un botón
+          IconButton(
+              //todo: Función
+              onPressed: () {},
+              //Icono
+              icon: const Icon(Icons.refresh_rounded))
+        ],
       ),
       //Cuerpo
-      body: const Center(child: Text('Counter Value: xxxxx')),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //El texto donde estará el valor númerico
+          const Text('Counter Value: xxxxx'),
+
+          //Botónes
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //Botón Sumar
+                FloatingActionButton(
+                  //
+                  heroTag: '1',
+                  //todo: Función
+                  onPressed: () {},
+                  child: const Text('+1'),
+                ),
+
+                //Botón Sumar
+                FloatingActionButton(
+                  //
+                  heroTag: '2',
+                  //todo: Función
+                  onPressed: () {},
+                  child: const Text('+2'),
+                ),
+
+                //Botón Sumar
+                FloatingActionButton(
+                  //
+                  heroTag: '3',
+                  //todo: Función
+                  onPressed: () {},
+                  child: const Text('+3'),
+                ),
+              ],
+            ),
+          ),
+
+          //Botón Refresh
+          FloatingActionButton(
+            //
+            heroTag: '4',
+            //todo: Función
+            onPressed: () {},
+            child: const Icon(Icons.refresh_rounded),
+          ),
+        ],
+      )),
+
+      //Botón Flotante
     );
   }
 }
