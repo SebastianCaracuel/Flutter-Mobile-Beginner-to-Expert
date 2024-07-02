7ce89175df681b550d36bd0478ef87934b52afc6
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-02 12:25:59 -0400

    Update custom_text_form_fiel.dart desing
    Actualizamos la pantalla, añadiendo el diseño deseado a nuestro campo de texto personalizado (CustomTextFormField).

--- 09_FormsApp/forms_app/lib/presentation/Widgets/inputs/custom_text_form_field.dart
+++ 09_FormsApp/forms_app/lib/presentation/Widgets/inputs/custom_text_form_field.dart
@@ -52,12 +52,41 @@ class CustomTextFormField extends StatelessWidget {
       },
       //Esto es para la decoración del campo de texto
       decoration: InputDecoration(
-          //Llamamos a utilizar la variable que definimos arriba para que el campo tenga un borde
-          enabledBorder: border,
-          //Le añadimos un color al borde, cuando esta activo es decir cuando se esta escribiendo en el TextFormField
-          focusedBorder:
-              //El color utilizado es el color de la aplicación, un color morado.
-              border.copyWith(borderSide: BorderSide(color: colors.primary))),
+        //Llamamos a utilizar la variable que definimos arriba para que el campo tenga un borde
+        enabledBorder: border,
+        //Le añadimos un color al borde, cuando esta activo es decir cuando se esta escribiendo en el TextFormField
+        focusedBorder:
+            //El color utilizado es el color de la aplicación, un color morado.
+            border.copyWith(borderSide: BorderSide(color: colors.primary)),
+        //Creamos un Diseño para el Error Border
+        errorBorder:
+            //El color utilizado es el color de la aplicación, un color rojo.
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+        // Creamos un Diseño para el Error Border.
+        focusedErrorBorder: //El color utilizado es el color de la aplicación, un color rojo.
+            border.copyWith(borderSide: BorderSide(color: Colors.red.shade800)),
+
+        //No queremos que el campo sea tan grande, Hace que el campo de texto sea más compacto
+        isDense: true,
+        //Proporciona una etiqueta que se muestra dentro del campo de texto cuando está vacío.
+        label: const Text('User'),
+        // Muestra un texto de sugerencia dentro del campo de texto cuando está vacío, proporcionando una pista al usuario sobre qué escribir
+        hintText: 'Name User',
+        // El color que se usa cuando el campo de texto tiene el foco (está seleccionado).
+        focusColor: colors.primary,
+        //Esto es un indicador, le indica al Usuario que hay un error en el campo de Texto
+        errorText: 'Este es el error text',
+        //Utilizamos un PreFixIcon que nos permite tener un Icono dentro de nuestro
+        prefixIcon: Icon(
+            //Le añadimos un Icono referente al campo
+            Icons.supervised_user_circle_rounded,
+            //Añadimos el color primario de la aplicación
+            color: colors.primary),
+        // ? Este es un Icono que nos permite colocar dentro de nuestro campo de texto, pero al final
+        // suffixIcon: Icon(Icons.supervised_user_circle, color: colors.primary),
+        // ? Este es un Icono fuera del campo de texto
+        // icon: Icon(Icons.supervised_user_circle, color: colors.primary),
+      ),
     );
   }
 }
