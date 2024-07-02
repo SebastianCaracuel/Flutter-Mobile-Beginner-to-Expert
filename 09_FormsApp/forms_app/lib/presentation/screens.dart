8b495059800912c62a568e7bed7f12108928148b
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-01 23:00:11 -0400

    Create screens.dart
    Copiamos y Pegamos el archivo de barril ya que seguiremos utilizando el código de la sección pasada.

---
+++ 09_FormsApp/forms_app/lib/presentation/screens.dart
@@ -0,0 +1,4 @@
+//? Este es un archivo de barril donde estarán todas las Screens Exportadas.
+export 'screens/bloc_counter_screen.dart';
+export 'screens/cubits_counter_screen.dart';
+export 'screens/home_screen.dart';
