28e358096c6dc3b2f02460216b6c3f21d0ae1745
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-02 11:27:19 -0400

    Create Widget custom_text_form_field.dart
    Creamos un Widget para el diseño del TextFormField o campo de texto y lo exportamos en un archivo de barril.

---
+++ 09_FormsApp/forms_app/lib/presentation/Widgets/inputs/custom_text_form_field.dart
@@ -0,0 +1,23 @@
+//Importaciones flutter
+import 'package:flutter/material.dart';
+
+//Importaciones nuestras
+
+//? Este será un Widget, del diseño del TextFormField
+
+//Creamos la clase
+class CustomTextFormField extends StatelessWidget {
+  //Propiedades
+
+  //Constructor
+  const CustomTextFormField({super.key});
+
+  //Objeto
+  @override
+  Widget build(BuildContext context) {
+    //Propiedades del Objeti
+
+    //!Widget Padre
+    return TextFormField();
+  }
+}
---
+++ 09_FormsApp/forms_app/lib/presentation/Widgets/widgets.dart
@@ -0,0 +1,2 @@
+//? Este es un archivo de barril donde estarán todos nuestros Widgets exportados
+export 'inputs/custom_text_form_field.dart';
