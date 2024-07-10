ded4be07255704957184396c866569a0e2e22896
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-10 10:35:12 -0400

    Create username.dart
    Creamos un archivo para el Input Username, serán Inputs personalizados y le añadimos sus validaciones.

---
+++ 09_FormsApp/forms_app/lib/infrastructure/inputs/username.dart
@@ -0,0 +1,29 @@
+//Importaciones flutter
+import 'package:formz/formz.dart';
+
+// Define los errores de validación de la entrada
+enum UsernameError { empty, lenght }
+
+// Extiende FormzInput y proporciona el tipo de entrada y el tipo de error.
+class Username extends FormzInput<String, UsernameError> {
+  // Llama a super.pure para representar una entrada de formulario no modificada.
+  const Username.pure() : super.pure('');
+
+  // Llama a super.dirty para representar una entrada de formulario modificada.
+  const Username.dirty(String value) : super.dirty(value);
+
+  // Sobrescribe el validador para manejar la validación de un valor de entrada dado.
+  @override
+  UsernameError? validator(String value) {
+    //todo: validaciones
+
+    //Si el campo esta vació lanzamos un error
+    if (value.isEmpty || value.trim().isEmpty) return UsernameError.empty;
+
+    //si el campo es menos de 6 caracteres, mandamos el error
+    if (value.length < 6) return UsernameError.lenght;
+
+    //
+    return null;
+  }
+}
