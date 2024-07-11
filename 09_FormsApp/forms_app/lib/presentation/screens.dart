72c2216b972c3988d695a7c800ca843f58123765
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-11 00:01:09 -0400

    Update screen.dart
    Añadimos la nueva pantalla a nuestro archivo de barril.

--- 09_FormsApp/forms_app/lib/presentation/screens.dart
+++ 09_FormsApp/forms_app/lib/presentation/screens.dart
@@ -2,4 +2,5 @@
 export 'screens/counter/bloc_counter_screen.dart';
 export 'screens/counter/cubits_counter_screen.dart';
 export 'screens/home/home_screen.dart';
+export 'screens/home/tutorial_screen.dart';
 export 'screens/register/register_screen.dart';
