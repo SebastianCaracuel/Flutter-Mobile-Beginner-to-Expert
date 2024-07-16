3e8b0eb19e127b1eb3f41a9b7ba53c0e869ba968
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-15 22:46:33 -0400

    Update tutorial_screen.dart
    Actualizamos la pantalla de tutorial, y le añadimos un botón con animación de entrada, que lo llevará a la pagina de registro.

--- 09_FormsApp/forms_app/lib/presentation/screens/home/tutorial_screen.dart
+++ 09_FormsApp/forms_app/lib/presentation/screens/home/tutorial_screen.dart
@@ -2,6 +2,8 @@
 
 //Importaciones Flutter
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:animate_do/animate_do.dart';
 //Importaciones nuestras
 
 // ? Creamos una clase para el movimiento de la pantalla
@@ -22,15 +24,57 @@ final slides = <SlideInfo>[
   //Dos
   SlideInfo('Welcome To My Channel', '📱 Forms App for Beginners 👨🏽‍💻',
       'assets/tutorial/1.png'),
+  //Dos
+  SlideInfo('Welcome To My Channel', '📱 Forms App for Beginners 👨🏽‍💻',
+      'assets/tutorial/1.png'),
 ];
 
 //Creación de la pantalla
 
-class TutorialScreen extends StatelessWidget {
+class TutorialScreen extends StatefulWidget {
   //Propiedades de la clase
 
   //Constructor
   const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  //Propieades
+
+  //? CREAMOS UNA VARIABLE PARA EL CONTROLADOR
+  late final PageController pageviewController = PageController();
+
+  //? si ya termino de hacer scroll SE MUESTRA EL BOTÓN
+  bool endReached = false;
+
+//Iniciacion
+  @override
+  void initState() {
+    super.initState();
+    //
+    pageviewController.addListener(() {
+      final page = pageviewController.page ?? 0;
+      //Creamos la condición - la condicion dice que cuando la pagina este por la mitad o apunto de pasar a la final
+      //que se muestre el botón debido a que llegamos al final de las paginas
+
+      if (!endReached && page >= (slides.length - 1.5)) {
+        //Llamamos al set
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+//Cerramos
+  @override
+  void dispose() {
+    pageviewController.dispose();
+    super.dispose();
+  }
 
   //Objeto
   @override
@@ -40,15 +84,36 @@ class TutorialScreen extends StatelessWidget {
     //!Widget Padre
     return Scaffold(
       //Cuerpo
-      body: PageView(
-        //Colocamos una animación al scroll
-        physics: const BouncingScrollPhysics(),
-        children: slides
-            .map((slideData) => _Slide(
-                title: slideData.title,
-                caption: slideData.caption,
-                imageUrl: slideData.imageUrl))
-            .toList(),
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageviewController,
+            //Colocamos una animación al scroll
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map((slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl))
+                .toList(),
+          ),
+          //Botón de navegación
+          endReached
+              ? Positioned(
+                  right: 30,
+                  bottom: 30,
+                  child: FadeInRight(
+                    //le añadimos un delay
+                    delay: const Duration(milliseconds: 1),
+                    //
+                    from: 15,
+                    child: FilledButton(
+                      onPressed: () => context.push('/new-user'),
+                      child: const Text('Start Now'),
+                    ),
+                  ))
+              : const SizedBox(),
+        ],
       ),
     );
   }
@@ -72,7 +137,52 @@ class _Slide extends StatelessWidget {
   Widget build(BuildContext context) {
     //Propiedades del Objeto
 
+    //? Opciones de tamaño para el titulo
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    //? Opciones de tamaño para el caption
+    final captionStyle = Theme.of(context).textTheme.titleMedium;
+
     //!Widget Hijo
-    return const Placeholder();
+    return SizedBox.expand(
+      //Utilizamos un expanded para que la imagen tome toda la pantalla
+      child: Stack(
+        //Utilizamos un Stack para que puedan haber otros objetos encima de la imagen
+        children: [
+          //Colocamos la imange y le damos un Fit.cover para que ocupe todo el espacio
+          Positioned.fill(
+            child: Image(
+              image: AssetImage(imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          //Colocamos un container para poder utilizar un gradinete
+          Container(
+            //Decoramos el contenedor con el gradiente
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.black.withOpacity(0.6), Colors.transparent],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.bottomLeft),
+            ),
+            //Centramos todos los objetos que esten dentro del contenedor
+            child: Center(
+              //Añadimos una columna
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20),
+                  Text(title, style: titleStyle),
+                  const SizedBox(height: 20),
+                  Text(caption, style: captionStyle)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
