043c465e6525017491748e5aea19e9c25251e4b3
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-17 21:10:24 -0400

    Push Notifications + Local Notifications
    Creamos la aplicación en Formato Empty (Vacio)

---
+++ 10_PushNotifications/push_notificaction_app/README.md
@@ -0,0 +1,3 @@
+# push_notificaction_app
+
+A new Flutter project.
---
+++ 10_PushNotifications/push_notificaction_app/android/app/src/main/kotlin/com/example/push_notificaction_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.push_notificaction_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
