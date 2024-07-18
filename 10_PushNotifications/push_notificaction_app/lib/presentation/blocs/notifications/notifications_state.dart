ebe746fb3e244cbfa2efac73ed66d81cb35f071f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-17 22:48:10 -0400

    Create Bloc
    Creamos los documentos de Bloc

---
+++ 10_PushNotifications/push_notificaction_app/lib/presentation/blocs/notifications/notifications_state.dart
@@ -0,0 +1,32 @@
+part of 'notifications_bloc.dart';
+
+class NotificationsState extends Equatable {
+  //Propieades de la clase
+
+  //? Creamos una variable que llama al paquete Firebase_messages
+  final AuthorizationStatus status;
+
+  //Creamos una lista
+  //todo: Terminar la lista - Crear mi modelo de notificaiones
+  final List<dynamic> notifications;
+
+  const NotificationsState({
+    this.status = AuthorizationStatus.notDetermined,
+    this.notifications = const [],
+  });
+
+//? Creamos un CopyWith de la clase
+  NotificationsState copyWith({
+    AuthorizationStatus? status,
+    List<dynamic>? notifications,
+  }) =>
+      NotificationsState(
+        status: status ?? this.status,
+        notifications: notifications ?? this.notifications,
+      );
+
+  @override
+  List<Object> get props => [status, notifications];
+}
+
+final class NotificationsInitial extends NotificationsState {}
