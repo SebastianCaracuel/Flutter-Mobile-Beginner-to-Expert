683fd21b562c39706eb7118a5bb7a2c469c1d49f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-17 23:41:53 -0400

    Update notifications_bloc.dart
    Actualizamos notificaciones bloc, creando un método para los permisos.

--- 10_PushNotifications/push_notificaction_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ 10_PushNotifications/push_notificaction_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -6,9 +6,31 @@ part 'notifications_event.dart';
 part 'notifications_state.dart';
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  //Propiedades del bLoC
+
+  //
   NotificationsBloc() : super(const NotificationsState()) {
     // on<NotificationsEvent>((event, emit) {
     // TODO: implement event handler
     // });
+  }
+
+//Creamos un método para los permisos
+  void requestPermission() async {
+    //?
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    //
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+
+    //
+    settings.authorizationStatus;
   }
 }
