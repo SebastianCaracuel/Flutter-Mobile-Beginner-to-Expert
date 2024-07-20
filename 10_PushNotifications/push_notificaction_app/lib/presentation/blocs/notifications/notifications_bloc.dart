deaa5c80a632e402c8aa1e713a63859f6eb5e0e1
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-20 10:56:35 -0400

    Update notifications_bloc.dart
    Determinamos el permiso actual, creamos un nuevo método, antes de que se inicie el Listener, este método tiene la variable para conocer el estado actual de la autorización.

--- 10_PushNotifications/push_notificaction_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ 10_PushNotifications/push_notificaction_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -12,10 +12,14 @@ part 'notifications_state.dart';
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   //Propiedades del bLoC
 
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
   //
   NotificationsBloc() : super(const NotificationsState()) {
     //Mandamos como referencia el método
     on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    //Llamamos al método de inicialización del Status
+    _initialStatusCheck();
   }
 
   //? Creamos un nuevo método que nos díra si cambio o no el estado de autorización
@@ -25,10 +29,17 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     emit(state.copyWith(status: event.status));
   }
 
+  //todo: creamos  un nuevo método, llamamos al método antes de que se inicie el LIstener
+  void _initialStatusCheck() async {
+    //Creamos una variable para los settings - para saber el estado actual.
+    final settings = await messaging.getNotificationSettings();
+    //Llamamos el nuevo evento con el settings notificaction
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
 //todo :Creamos un método para los permisos
   void requestPermission() async {
     //?
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
     //
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
