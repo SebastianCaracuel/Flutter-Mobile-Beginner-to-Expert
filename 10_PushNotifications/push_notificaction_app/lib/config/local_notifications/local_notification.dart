e49760b4e9bca6705b93e6a4a655cd5e3d0848ae
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-07-25 20:50:09 -0400

    Update local_notification.dart
    Colocamos todo en una nueva clase, para nuestras notificaciones locales.

--- 10_PushNotifications/push_notificaction_app/lib/config/local_notifications/local_notification.dart
+++ 10_PushNotifications/push_notificaction_app/lib/config/local_notifications/local_notification.dart
@@ -1,10 +1,13 @@
 //Importacion flutter
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-Future<void> requestPermissionLocalNotifications() async {
-  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.requestPermission();
+//Creamos una clase para las notificaciones locales
+class LocalNotifications {
+  static Future<void> requestPermissionLocalNotifications() async {
+    final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestPermission();
+  }
 }
