ad88b8b45589f3539cdf8132b24c55b7f55f30b1
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-06 07:30:14 -0400

    Update main.dart
    Inicializamos el Provider en nuestro archivo main para acceder a todos nuestros providers futuros.

--- 11_TesloShop_app/teslo_shop_app/lib/main.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/config.dart';
 import 'config/router/app_router.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+      //Esto es para inicializar el Provider (Riverpord)
+      const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
