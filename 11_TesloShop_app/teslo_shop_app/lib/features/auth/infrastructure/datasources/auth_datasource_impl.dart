1300576c84b4cdbcfa75d84bd7be4448bcdd99f3
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-08 22:06:25 -0400

    Update auth_datasource_impl.dart
    Implementamos nuestro Login, creamos una propiedad para llamar a nuestra Url.
    Utilizamos un TryCatch, Obtenemos por petición post los datos de autorización de logeo y implementamos los errores personalizados.

--- 11_TesloShop_app/teslo_shop_app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -10,10 +10,29 @@ import 'package:teslo_shop_app/features/auth/infrastructure/infrastructure.dart'
 
 //Esta es la clase con extensión de nuestro Datasource para ser implementado
 class AuthDatasourceImpl extends AuthDatasource {
+  //Propiedades
+
+  //? Creamos una propiedad para utilizar Dio (La dependencia)
+  final dio = Dio(
+      //Llamamos a nuestra URl
+      BaseOptions(baseUrl: Environment.apiUrl));
   @override
-  Future<User> login(String email, String password) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async {
+    //Realizamos un TryCatch para verificar errores
+    try {
+      //Cramos una variable, donde realizamos una petición post
+      final response = await dio.post(
+        '/auth/login',
+        data: {'email': email, 'password': password},
+      );
+      final user = UserMapper.userJsonToEntity(response.data);
+      return user;
+
+      //! Implementamos lo errores
+    } catch (e) {
+      //? Llamamos a nuestro error personalizado
+      throw WrongCredentials();
+    }
   }
 
   @override
