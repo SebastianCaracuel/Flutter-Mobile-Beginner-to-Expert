d37dd4fc665159dc50c2d56c4f7391d91cbe6e84
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-09 10:13:26 -0400

    Update auth_repositories.dart
    Correción de tipeo.

--- 11_TesloShop_app/teslo_shop_app/lib/features/auth/domain/repositories/auth_repositories.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/auth/domain/repositories/auth_repositories.dart
@@ -2,12 +2,7 @@
 import 'package:teslo_shop_app/features/auth/domain/domain.dart'; //Llamamos a nuestro usuario
 
 abstract class AuthRepository {
-  //? Este es para realizar el ingreso a la aplicación
   Future<User> login(String email, String password);
-
-  //?
   Future<User> register(String email, String password, String fullName);
-
-  //? Este es para identificar si la persona ya esta autenticada o no
   Future<User> checkAuthStatus(String token);
 }
