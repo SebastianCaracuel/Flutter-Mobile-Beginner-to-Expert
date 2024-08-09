af7774c29eaa54a231c31d0883d609f1b6a4f4a2
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-08 21:20:58 -0400

    Update main.dart
    Añadimos, nuestro assets en nuestro main, para poder utilizar correctamente ".env".

--- 11_TesloShop_app/teslo_shop_app/lib/main.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'config/config.dart';
 import 'config/router/app_router.dart';
+import 'config/config.dart';
 
-void main() {
+void main() async {
+  //Añadimos nuestro assets en nuestro main
+  await dotenv.load(fileName: ".env");
   runApp(
       //Esto es para inicializar el Provider (Riverpord)
       const ProviderScope(child: MainApp()));
