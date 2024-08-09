7cf110f8a5b55e1108d1039242e6221b1f262432
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-08 21:23:22 -0400

    Update main.dart
    Al realizar cambios en nuestro archivo de barril, podemos llamar a nuestra variable directamente por lo que no es necesario tener el código "dotenv.load( ) ..."

--- 11_TesloShop_app/teslo_shop_app/lib/main.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'config/router/app_router.dart';
 import 'config/config.dart';
 
 void main() async {
   //Añadimos nuestro assets en nuestro main
-  await dotenv.load(fileName: ".env");
+  await Environment.initEnvironment();
   runApp(
       //Esto es para inicializar el Provider (Riverpord)
       const ProviderScope(child: MainApp()));
