0b3a453cfe5a96528044bf3e0cfc8fd4a79c33dd
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-08 22:03:13 -0400

    Create auth_error.dart
    Creamos un archivo de errores personalizados, sin embargo aún no se realiza correctamente la implementación de estos errores.

---
+++ 11_TesloShop_app/teslo_shop_app/lib/features/auth/infrastructure/errors/auth_error.dart
@@ -0,0 +1,7 @@
+//? Creamos unos errores personalizados
+
+//todo: Creamos un error para las credenciales invalidas
+class WrongCredentials implements Exception {}
+
+//todo: Creamos un error para el Token invalido
+class InvalidToken implements Exception {}
