628d903465770ad9d6ec6e102cfe1edf139af324
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-09 10:13:16 -0400

    Update user.dart
    Corrección de Tipeo.

--- 11_TesloShop_app/teslo_shop_app/lib/features/auth/domain/entities/user.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/auth/domain/entities/user.dart
@@ -1,22 +1,19 @@
 //? Creamos a nuestro usuario
+
 class User {
-  //Propiedades del usuario
   final String id;
   final String email;
   final String fullName;
   final List<String> roles;
   final String token;
 
-  //Constructor
-  User({
-    required this.id,
-    required this.email,
-    required this.fullName,
-    required this.roles,
-    required this.token,
-  });
+  User(
+      {required this.id,
+      required this.email,
+      required this.fullName,
+      required this.roles,
+      required this.token});
 
-  //? Identificamos si es administrador con una variable booleana
   bool get isAdmin {
     return roles.contains('admin');
   }
