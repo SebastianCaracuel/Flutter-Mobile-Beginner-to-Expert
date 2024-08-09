8df5b1088795bbf04fcacb1764198a4d11878895
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-08 21:20:32 -0400

    Update config.dart
    Actualizamos nuestro archivo de barril de las configuraciones de la aplicación, y añadimos el nuevo archivo de nuestras variables constantes.

--- 11_TesloShop_app/teslo_shop_app/lib/config/config.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/config/config.dart
@@ -1,3 +1,4 @@
-
-
+//? Este es un archivo barril de todas las configuraciones de la aplicación
+export 'constants/environment.dart';
+export 'router/app_router.dart';
 export 'theme/app_theme.dart';
