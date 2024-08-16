4a70b5be604fdc79866acfdab7a469085f3e4a1f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-16 16:35:40 -0400

    Update auth_provider.dart
    Realizamos unas correcciones para evitar código de más, por lo que ahora el código esta más optimizado y no se repiten errores, y solamente mandamos un error personalizado por pantalla al usuario.

--- 11_TesloShop_app/teslo_shop_app/lib/features/auth/presentation/providers/auth/auth_provider.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/auth/presentation/providers/auth/auth_provider.dart
@@ -68,10 +68,8 @@ class AuthNotifier extends StateNotifier<AuthState> {
     try {
       final user = await authRepository.login(email, password);
       _setLoggedUser(user);
-    } on WrongCredentials {
-      logout('Credenciales no son Correctas');
-    } on ConnectionTimeOut {
-      logout('TimeOut');
+    } on CustomError catch (e) {
+      logout(e.message);
     } catch (e) {
       logout('Error no controlado');
     }
