9ad07164387c2d885c2a3f92eaebe5777c371462
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-16 14:06:02 -0400

    Update auth_error.dart
    Creamos un error personalizado y creamos un error para el tiempo de conexión.

--- 11_TesloShop_app/teslo_shop_app/lib/features/auth/infrastructure/errors/auth_error.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/auth/infrastructure/errors/auth_error.dart
@@ -1,7 +1,21 @@
 //? Creamos unos errores personalizados
 
-//todo: Creamos un error para las credenciales invalidas
-class WrongCredentials implements Exception {}
+//todo: Creamos un error para el tiempo de conexión
+class ConnectionTimeOut implements Exception {}
 
 //todo: Creamos un error para el Token invalido
 class InvalidToken implements Exception {}
+
+//todo: Creamos un error para las credenciales invalidas
+class WrongCredentials implements Exception {}
+
+//? Creamos un error personalizado
+class CustomError implements Exception {
+  //Propiedades
+
+  final String message;
+  final int errorCode;
+
+  //Constructor
+  CustomError(this.message, this.errorCode);
+}
