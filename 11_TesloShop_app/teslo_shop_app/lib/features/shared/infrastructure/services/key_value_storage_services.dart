edf373025134f248939c9f8eb68a396d9e07cb39
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-17 14:37:16 -0400

    Create key_value_storage_services.dart
    Este es un archivo para crear una clase de almacenamiento de datos basados en pares de clave- valor. Donde creamos 3 métodos asíncronos, uno se utiliza para guardar un valor asociado a una clave, otro para obtener un valor asociado a una clave y el otro para eliminar la clave.

---
+++ 11_TesloShop_app/teslo_shop_app/lib/features/shared/infrastructure/services/key_value_storage_services.dart
@@ -0,0 +1,18 @@
+//? Este es un archivo para un servicio de almacenamiento de datos basado en pares clave-valor
+
+abstract class KeyValueStorageServices {
+  //? Este método es asíncrono y se utiliza para guardar un valor (value)
+  //? asociado a una clave (key) en el almacenamiento.
+  //? No devuelve nada (void).
+  Future<void> setKeyValue<T>(String key, T value);
+
+  //? Este método es asíncrono y se utiliza para obtener un valor almacenado
+  //? asociado a una clave (key). Devuelve un Future que contiene el valor.
+  //? El tipo de retorno debería ser especificado según la implementación.
+  Future<T?> getValue<T>(String key);
+
+  //? Este método es asíncrono y se utiliza para eliminar una clave (key)
+  //? y su valor asociado del almacenamiento. Devuelve un Future<bool>
+  //? que indica si la eliminación fue exitosa (true) o no (false).
+  Future<bool> removeKey(String key);
+}
