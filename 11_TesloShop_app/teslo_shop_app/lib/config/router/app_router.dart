69a886b56a88c6f978821130b08b263d1e83ca7d
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-17 17:35:17 -0400

    Update app_router.dart
    Actualizamos el Go_Router, para poder trabajar con la protección de rutas.
    
    Creando un Go_Router con provider, para utilizar posteriormente un Redirect.

--- 11_TesloShop_app/teslo_shop_app/lib/config/router/app_router.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/config/router/app_router.dart
@@ -1,35 +1,43 @@
 //Importaciones flutter
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 //Importaciones Nuestras
 import 'package:teslo_shop_app/features/auth/auth.dart';
 import 'package:teslo_shop_app/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    //Primera pantalla
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
+//Realizamos todo el Go Router con provider
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      //Primera pantalla
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+      ///* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+    //Autenticación con redirect
+    redirect: (context, state) {
+      //return '/';
+      return null;
+    },
+  );
+});
