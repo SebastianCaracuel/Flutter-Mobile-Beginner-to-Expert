88d3f88b840848f25b2637fd3bba7e944df82fd7
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-29 19:13:42 -0400

    Update products_provider.dart
    Riverpod - StateNotifierProvider - Notifier
    
    Creamos una clase que extiende de StateNotifier, con el estado "ProductsState".
    
    Lo que nos permite es, mediante métodos, cargar la siguiente página de productos, actualiza el estado para indiciar que se está cargando, y con unas respectivas validaciones.

--- 11_TesloShop_app/teslo_shop_app/lib/features/products/presentation/providers/products_provider.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/presentation/providers/products_provider.dart
@@ -1,7 +1,55 @@
 // Importamos las definiciones del dominio, que probablemente incluye la clase 'Producto'
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop_app/features/products/domain/domain.dart';
 
-// Clase que define el estado de los productos en el proveedor
+//todo: Definimos la clase 'ProductsNotifier', que extiende 'StateNotifier' con un estado de tipo 'ProductsState'
+class ProductsNotifier extends StateNotifier<ProductsState> {
+  // Dependencia del repositorio de productos, que se utilizará para interactuar con los datos de los productos
+  final ProductsRepository productsRepository;
+
+  // Constructor para 'ProductsNotifier'
+  // Requiere una instancia de 'ProductsRepository' para inicializarse
+  // Llama al constructor de la superclase 'StateNotifier' con un estado inicial de 'ProductsState'
+  // Además, invoca el método 'loadNextPage()' para cargar la primera página de productos
+  ProductsNotifier({required this.productsRepository})
+      : super(ProductsState()) {
+    loadNextPage();
+  }
+
+  // Método asíncrono que carga la siguiente página de productos
+  Future loadNextPage() async {
+    // Si ya se está cargando o si es la última página, no se hace nada
+    if (state.isLoading || state.isLastPage) return;
+
+    // Actualiza el estado para indicar que se está cargando
+    state = state.copyWith(
+      isLoading: true,
+    );
+
+    // Llama al repositorio para obtener la siguiente página de productos
+    final products = await productsRepository.getProductsByPage(
+        limit: state.limit, offset: state.offset);
+
+    // Si no se obtuvieron productos, se marca como la última página y se detiene la carga
+    if (products.isEmpty) {
+      state = state.copyWith(
+        isLoading: false,
+        isLastPage: true,
+      );
+      return;
+    }
+
+    // Si se obtuvieron productos, actualiza el estado con los nuevos productos,
+    // desactiva el indicador de carga, actualiza el offset, y marca que no es la última página
+    state = state.copyWith(
+        isLastPage: false,
+        isLoading: false,
+        offset: state.offset + 10,
+        products: [...state.products, ...products]);
+  }
+}
+
+//todo: Clase que define el estado de los productos en el proveedor
 class ProductsState {
   // Indica si la página actual es la última página de productos
   final bool isLastPage;
@@ -30,13 +78,13 @@ class ProductsState {
 
   // Método 'copyWith' para crear una nueva instancia de 'ProductsState'
   // Permite copiar el estado existente y sobrescribir propiedades específicas
-  ProductsState copyWith(
+  ProductsState copyWith({
     bool? isLastPage, // Permite cambiar si es la última página
     int? limit, // Permite cambiar el límite de productos
     int? offset, // Permite cambiar el desplazamiento
     bool? isLoading, // Permite cambiar el estado de carga
-    List<Producto>? products, // Permite cambiar la lista de productos
-  ) =>
+    List<Producto>? products,
+  }) => // Permite cambiar la lista de productos
       // Retorna una nueva instancia de 'ProductsState' con las propiedades
       // sobrescritas o los valores actuales si no se proporcionan nuevos valores
       ProductsState(
