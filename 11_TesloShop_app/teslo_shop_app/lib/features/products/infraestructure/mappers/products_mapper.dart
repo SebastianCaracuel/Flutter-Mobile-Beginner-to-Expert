d4defdaa84d8dde06f7d62bd48df25157cff5bbc
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-29 18:33:17 -0400

    Create products_mapper.dart
    Creamos nuestro mapper del producto.

---
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/infraestructure/mappers/products_mapper.dart
@@ -0,0 +1,28 @@
+//? Este es un archivo para mapear el producto
+
+//Importaciones Flutter
+
+//Importaciones nuestras
+import 'package:teslo_shop_app/config/config.dart';
+import 'package:teslo_shop_app/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop_app/features/products/domain/domain.dart';
+
+class ProductMapper {
+  static jsonToEntity(Map<String, dynamic> json) => Producto(
+        id: json['id'],
+        title: json['title'],
+        price: double.parse(json['price'].toString()),
+        description: json['description'],
+        slug: json['slug'],
+        stock: json['stock'],
+        sizes: List<String>.from(json['sizes'].map((size) => (size))),
+        gender: json['gender'],
+        tags: List<String>.from(json['sizes'].map((size) => (size))),
+        images: List<String>.from(
+          json['image'].map((String image) => image.startsWith('http')
+              ? image
+              : '${Environment.apiUrl}/files/product/$image'),
+        ),
+        user: UserMapper.userJsonToEntity(json['user']),
+      );
+}
