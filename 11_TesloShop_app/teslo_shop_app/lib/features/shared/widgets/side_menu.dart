785ed15585dd6209af228c04a9e7a52257a35a27
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-29 17:27:26 -0400

    Update side_menu.dart
    Eliminamos una importaciones que no se esta utilizando en nuestro código y nos da un pequeño "error".

--- 11_TesloShop_app/teslo_shop_app/lib/features/shared/widgets/side_menu.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/shared/widgets/side_menu.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:teslo_shop_app/features/auth/presentation/providers/auth/auth_provider.dart';
 import 'package:teslo_shop_app/features/shared/shared.dart';
 
