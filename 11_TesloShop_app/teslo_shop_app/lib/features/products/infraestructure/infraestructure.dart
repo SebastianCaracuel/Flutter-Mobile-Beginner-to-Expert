cd87c2a4c9ec2b755926fad9d66375c2cbc88806
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-29 18:36:49 -0400

    Create infraestructure.dart
    Creamos un archivo de barril para nuestra infraestructura del producto.

---
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/infraestructure/infraestructure.dart
@@ -0,0 +1,4 @@
+//? Este es un archivo de barril de Infraestructura del producto
+export './datasources/products_datasource_impl.dart';
+export './mappers/products_mapper.dart';
+export './repositories/products_repository_impl.dart';
