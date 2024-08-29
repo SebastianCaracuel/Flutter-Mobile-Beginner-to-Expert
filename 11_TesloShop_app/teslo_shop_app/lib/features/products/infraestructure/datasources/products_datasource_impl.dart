0ee87ae6f2a282fd68c5ae2f6439a358a47c47d7
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-29 18:14:53 -0400

    Update products_datasource_impl.dart
    Realizamos la implementación del "getProductsByPage", sin embargo está incompleta debido que falta el Mapper

--- 11_TesloShop_app/teslo_shop_app/lib/features/products/infraestructure/datasources/products_datasource_impl.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/infraestructure/datasources/products_datasource_impl.dart
@@ -1,9 +1,23 @@
 //? Este es un archivo de implementación del datasource
 
+//Importaciones Flutter
+import 'package:dio/dio.dart';
+
 //Importaciones nuestras
+import 'package:teslo_shop_app/config/config.dart';
 import 'package:teslo_shop_app/features/products/domain/domain.dart';
 
 class ProductsDatasourceImpl extends ProductsDatasource {
+  //Utilizamos DIO
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDatasourceImpl({required this.accessToken})
+      : dio = Dio(BaseOptions(
+          baseUrl: Environment.apiUrl,
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ));
+
   @override
   Future<Producto> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -17,9 +31,17 @@ class ProductsDatasourceImpl extends ProductsDatasource {
   }
 
   @override
-  Future<List<Producto>> getProductsByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductsByPage
-    throw UnimplementedError();
+  Future<List<Producto>> getProductsByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response =
+        await dio.get<List>('/api/products?limit=$limit&offset=$offset');
+
+    //Producto
+    final List<Producto> products = [];
+    for (final product in response.data ?? []) {
+      //products.add(value) //mapper
+    }
+    return products;
   }
 
   @override
