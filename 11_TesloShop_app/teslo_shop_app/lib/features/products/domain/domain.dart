8015383d4c7dcb1d1136d6080dfc9ce69b58d3d5
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-08-29 17:56:01 -0400

    Create domain.dart
    Creamos un archivo de barril de nuestro dominio del producto.

---
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/domain/domain.dart
@@ -0,0 +1,4 @@
+//? Este es un archivo de barril del domain de nuestro producto
+export './datasource/products_datasource.dart';
+export './entities/productos.dart';
+export './repositories/producto_repositories.dart';
