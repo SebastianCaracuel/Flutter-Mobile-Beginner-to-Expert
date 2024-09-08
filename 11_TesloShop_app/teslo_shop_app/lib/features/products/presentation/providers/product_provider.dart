bdd6e6cbee564e22c55f5277ec346271b4043c07
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-09-08 13:44:27 -0300

    Create product_provider.dart
    Creamos un nuevo archivo llamado product_provider.dart que, nos permite gestionar el estado y las operaciones de un producto específico en la aplicación, permitiendo cargar y modificar, productos a través de una "StateNotifier" con Riverpod.

---
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/presentation/providers/product_provider.dart
@@ -0,0 +1,73 @@
+// Este archivo define un proveedor y un Notifier para ver o editar un producto en particular
+
+// Importamos las utilidades necesarias de Flutter Riverpod
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Importamos nuestro proveedor de repositorio de productos
+import './products_repository_provider.dart';
+// Importamos nuestras entidades del dominio, que probablemente incluyen 'Producto'
+import 'package:teslo_shop_app/features/products/domain/domain.dart';
+
+// todo: Creamos un StateNotifierProvider utilizando el modificador 'autoDispose' y 'family'
+// autoDispose: permite liberar los recursos cuando el proveedor ya no se necesita
+// family: permite pasar parámetros (en este caso 'productId') al proveedor
+final productProvider = StateNotifierProvider.autoDispose
+    .family<ProductNotifier, ProductState, String>((ref, productId) {
+  // Obtenemos el repositorio de productos usando 'productsRepositoryProvider'
+  final productsRepository = ref.watch(productsRepositoryProvider);
+
+  // Retornamos una instancia de 'ProductNotifier' pasando el repositorio y el 'productId'
+  return ProductNotifier(
+      productsRepository: productsRepository, productId: productId);
+});
+
+//todo: Notificador que gestionará el estado relacionado con un producto específico
+class ProductNotifier extends StateNotifier<ProductState> {
+  // Dependencia del repositorio de productos
+  final ProductsRepository productsRepository;
+
+  // Constructor de 'ProductNotifier'
+  // Recibe el repositorio de productos y el 'productId' para inicializar el estado
+  ProductNotifier({
+    required this.productsRepository,
+    required String productId,
+  }) : super(ProductState(id: productId));
+
+  // Método que permitirá cargar el producto desde el repositorio
+  // Por ahora no hace nada, pero eventualmente contendrá la lógica de carga del producto
+  Future<void> loadProduct() async {}
+}
+
+// todo: Clase que define el estado del producto
+class ProductState {
+  // Propiedades del estado de un producto
+  final String id; // Identificador del producto
+  final Producto?
+      product; // El producto en sí, puede ser nulo si no ha sido cargado
+  final bool isLoading; // Indica si se está cargando el producto
+  final bool isSaving; // Indica si se está guardando el producto
+
+  // Constructor que inicializa las propiedades del estado
+  // Por defecto, el producto está en modo 'isLoading' al crearse el estado
+  ProductState(
+      {required this.id,
+      this.product,
+      this.isLoading = true, // Por defecto, se considera que está cargando
+      this.isSaving = false}); // Por defecto, no está guardando cambios
+
+  // Método 'copywith' que permite crear una copia del estado actual
+  // Permite cambiar algunas propiedades sin modificar el estado original
+  ProductState copywith({
+    String? id,
+    Producto? product,
+    bool? isLoading,
+    bool? isSaving,
+  }) =>
+      // Retorna una nueva instancia de 'ProductState' con las propiedades modificadas
+      ProductState(
+        id: id ?? this.id,
+        product: product ?? this.product,
+        isLoading: isLoading ?? this.isLoading,
+        isSaving: isSaving ?? this.isSaving,
+      );
+}
