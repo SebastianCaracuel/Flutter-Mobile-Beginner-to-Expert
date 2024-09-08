93333036808817bcd328c9f3f1e93037e8b89972
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-09-08 14:00:01 -0300

    Update infraestructure.dart
    Importamos nuestro errores a nuestro archivo de barril.

--- 11_TesloShop_app/teslo_shop_app/lib/features/products/infraestructure/infraestructure.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/infraestructure/infraestructure.dart
@@ -1,4 +1,5 @@
 //? Este es un archivo de barril de Infraestructura del producto
 export './datasources/products_datasource_impl.dart';
+export './errors/product_errors.dart';
 export './mappers/products_mapper.dart';
 export './repositories/products_repository_impl.dart';
