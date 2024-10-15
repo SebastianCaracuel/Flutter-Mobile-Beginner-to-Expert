7f963307dfab106337481142bb89c686d6f7f151
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-15 11:41:05 -0300

    product_screen.dart
    Le añadimos un GestureDetector a nuestro product_screen, para ocultar el teclado cuando ya no se necesita o no se estará utilizando con "FocusScope.of(context).unfocus();"

--- 11_TesloShop_app/teslo_shop_app/lib/features/products/presentation/screens/product_screen.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/products/presentation/screens/product_screen.dart
@@ -33,31 +33,40 @@ class ProductScreen extends ConsumerWidget {
     final productState = ref.watch(productProvider(productId));
 
     //!Widget padre
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Editar Producto'),
-        actions: [
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined))
-        ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Editar Producto'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  //Ocultamos el teclado al darle al botón guardar.
+                  FocusScope.of(context).unfocus();
+                },
+                icon: const Icon(Icons.camera_alt_outlined))
+          ],
+        ),
+        body: productState.isLoading
+            ? const FullScreenLoader()
+            : _ProductView(product: productState.product!),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //Ocultamos el teclado al darle al botón guardar.
+              FocusScope.of(context).unfocus();
+              //Condición, si no encuentra el producto, entonces no hace nada
+              if (productState.product == null) return;
+              //
+              ref
+                  .read(productFormProvider(productState.product!).notifier)
+                  .onFormSubmit()
+                  .then((value) {
+                if (!value) return;
+                showSnackbar(context);
+              });
+            },
+            child: const Icon(Icons.save_as_outlined)),
       ),
-      body: productState.isLoading
-          ? const FullScreenLoader()
-          : _ProductView(product: productState.product!),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            //Condición, si no encuentra el producto, entonces no hace nada
-            if (productState.product == null) return;
-            //
-            ref
-                .read(productFormProvider(productState.product!).notifier)
-                .onFormSubmit()
-                .then((value) {
-              if (!value) return;
-              showSnackbar(context);
-            });
-          },
-          child: const Icon(Icons.save_as_outlined)),
     );
   }
 }
@@ -220,6 +229,8 @@ class _SizeSelector extends StatelessWidget {
       }).toList(),
       selected: Set.from(selectedSizes),
       onSelectionChanged: (newSelection) {
+        //Ocultamos el teclado al darle al botón guardar.
+        FocusScope.of(context).unfocus();
         onSizesChanged(List.from(newSelection));
       },
       multiSelectionEnabled: true,
@@ -256,6 +267,8 @@ class _GenderSelector extends StatelessWidget {
         }).toList(),
         selected: {selectedGender},
         onSelectionChanged: (newSelection) {
+          //Ocultamos el teclado al darle al botón guardar.
+          FocusScope.of(context).unfocus();
           onGenderChanged(newSelection.first);
         },
       ),
