234b396130edc8c3227529838da1fcdab45e8c20
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-15 13:34:55 -0300

    services.dart
    Actualizamos services.dart, exportando los nuevos servicios de la camara.

--- 11_TesloShop_app/teslo_shop_app/lib/features/shared/infrastructure/services/services.dart
+++ 11_TesloShop_app/teslo_shop_app/lib/features/shared/infrastructure/services/services.dart
@@ -1,3 +1,5 @@
 //? Este es un archivo de barril para colocar todos los servicios
+export 'camera/camera_gallery_service_impl.dart';
+export 'camera/camera_gallery_service.dart';
 export 'key_value_storage_services_impl.dart';
 export 'key_value_storage_services.dart';
