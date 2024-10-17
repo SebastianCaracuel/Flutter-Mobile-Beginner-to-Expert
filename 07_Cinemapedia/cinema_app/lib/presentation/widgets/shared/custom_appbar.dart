0812c041f5f285875d0c480264e21d4277cd4eed
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-16 21:48:04 -0300

    custom_appbar.dart
    Se quito una importación debido a que no era necesaria, ya que había una importación con el archivo de barril.

--- 07_Cinemapedia/cinema_app/lib/presentation/widgets/shared/custom_appbar.dart
+++ 07_Cinemapedia/cinema_app/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,5 +1,4 @@
 //Importaciones Flutter
-import 'package:cinema_app/presentation/providers/theme/theme_provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
