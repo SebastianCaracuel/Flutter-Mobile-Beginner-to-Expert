8ced72ed872036c809df38aa56ec6d5ade836255
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 17:35:17 -0300

    header_cuadrado.dart
    Creamos el Header cuadrado, aún no se utiliza el custom painter.

---
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/header_cuadrado.dart
@@ -0,0 +1,17 @@
+//? Este es un archivo donde se crearán los estilos de Headers
+
+//Importaciones
+import 'package:flutter/material.dart';
+
+//?Creamos el Header Cuadrado
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: const Color(0xff615AAB),
+    );
+  }
+}
