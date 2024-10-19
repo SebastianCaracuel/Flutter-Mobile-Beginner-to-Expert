9a4915ce72b428fc006e183b84715698d8bc796f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 18:35:18 -0300

    header_pico.dart
    Creamos un nuevo Widget, utilizando custom painter, con el estilo Header pico.

---
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/header_pico.dart
@@ -0,0 +1,56 @@
+//Importaciones Flutter
+import 'package:flutter/material.dart';
+
+//?Creamos el Header Diagonal
+class HeaderPico extends StatelessWidget {
+  const HeaderPico({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+
+      //todo:Custom Painter
+      child: CustomPaint(
+        painter: _HeaderPicoPainter(),
+      ),
+    );
+  }
+}
+
+//? Creamos una nueva clase, pero extendiendo con el Custom Painter
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Creamos una variable "El lapiz para pintar
+    final lapiz = Paint();
+
+    //?Propiedades del Lapiz
+    lapiz.color = const Color(0xff615AAB);
+
+    //?¿Que tipo de rellenado quiero?
+    // lapiz.style = PaintingStyle.stroke; //Son solo los bordes
+    lapiz.style = PaintingStyle.fill; //Rellena todo
+
+    //?¿Que tan ancho quiero que sea ese lapiz?
+    lapiz.strokeWidth = 20;
+
+    //?¿Como queremos mover el lapiz?
+    final path = Path();
+
+    //todo: Con esto Dibujamos
+    path.lineTo(0, size.height * 0.20);
+    path.lineTo(size.width * 0.5, size.height * 0.3);
+    path.lineTo(size.width, size.height * 0.20);
+    path.lineTo(size.width, 0);
+
+    //?Le decimos al canvas que dibuje el Path, usando el lapiz
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
