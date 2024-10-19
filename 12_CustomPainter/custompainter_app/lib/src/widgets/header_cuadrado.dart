139f124ce2060002c61f6b797d1827c9eecb8092
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 17:44:24 -0300

    header_cuadrado.dart
    Se actualiza el header_cuadrado.dart, añadiendo un nuevo Header, que es un Header Cuadrado pero con bordes redondenados.

--- 12_CustomPainter/custompainter_app/lib/src/widgets/header_cuadrado.dart
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/header_cuadrado.dart
@@ -15,3 +15,21 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+//?Creamos el Header Cuadrado con bordes redondeados
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(50),
+            bottomRight: Radius.circular(50),
+          )),
+    );
+  }
+}
