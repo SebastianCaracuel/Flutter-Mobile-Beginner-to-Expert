3fc3418fc6a445c94a35e4789e9eaa37c41926f0
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 18:46:42 -0300

    widgets.dart
    Actualizamos el archivo de barril, añadiendo un nuevo Widget, el Widget de header wave o Header ola.

--- 12_CustomPainter/custompainter_app/lib/src/widgets/widgets.dart
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/widgets.dart
@@ -1,7 +1,7 @@
 //? Este es un archivo de barril
-
 export 'header_cuadrado.dart';
-export 'header_diagonal.dart';
-export 'header_triangulo.dart';
-export 'header_pico.dart';
 export 'header_curvo.dart';
+export 'header_diagonal.dart';
+export 'header_pico.dart';
+export 'header_triangulo.dart';
+export 'header_waves.dart';
