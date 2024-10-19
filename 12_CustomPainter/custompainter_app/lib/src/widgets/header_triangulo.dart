c2e891e0817a6896721e18e27694cb83a49ad23f
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 18:20:19 -0300

    header_triangulo.dart
    Creamos un nuevo Widget, llamado header_triangulo.dart, para saber como realizar un Header en forma triangular.

---
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/header_triangulo.dart
@@ -0,0 +1,60 @@
+//Importaciones Flutter
+import 'package:flutter/material.dart';
+
+//?Creamos el Header Diagonal
+class HeaderTriangulo extends StatelessWidget {
+  const HeaderTriangulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+
+      //todo:Custom Painter
+      child: CustomPaint(
+        painter: _HeaderTrianguloPainter(),
+      ),
+    );
+  }
+}
+
+//? Creamos una nueva clase, pero extendiendo con el Custom Painter
+class _HeaderTrianguloPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Creamos una variable "El lapiz para pintar
+    final lapiz = Paint();
+
+    //?Propiedades del Lapiz
+    lapiz.color = const Color(0xff615AAB);
+
+    //?¿Que tipo de rellenado quiero?
+    //lapiz.style = PaintingStyle.stroke; //Son solo los bordes
+    lapiz.style = PaintingStyle.fill; //Rellena todo
+
+    //?¿Que tan ancho quiero que sea ese lapiz?
+    lapiz.strokeWidth = 2;
+
+    //?¿Como queremos mover el lapiz?
+    final path = Path();
+
+    //todo: Con esto Dibujamos un triangulo Pintando la parte de arriba
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+    path.lineTo(0, 0);
+
+    //todo:Con esto Dibujamos un triangulo Pintando la parte de abajo
+    // path.lineTo(size.width, size.height);
+    // path.lineTo(0, size.height);
+    // path.lineTo(0, 0);
+
+    //?Le decimos al canvas que dibuje el Path, usando el lapiz
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
