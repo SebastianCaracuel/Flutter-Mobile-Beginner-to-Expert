732159e4bfcf9eac3b4e0e36e51117a16d30aaee
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 19:27:50 -0300

    widgets.dart
    Actualizamos el archivo de barril widgets.dart, añadiendo un nuevo widget pero con gradiente.

--- 12_CustomPainter/custompainter_app/lib/src/widgets/widgets.dart
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/widgets.dart
@@ -2,6 +2,7 @@
 export 'header_cuadrado.dart';
 export 'header_curvo.dart';
 export 'header_diagonal.dart';
+export 'header_gradient.dart';
 export 'header_pico.dart';
 export 'header_triangulo.dart';
 export 'header_waves.dart';
