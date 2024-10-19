d41300332bd45f47153141c6492bb1dbd708fe3a
SebastianCaracuel <163356372+SebastianCaracuel@users.noreply.github.com>
2024-10-19 18:06:14 -0300

    header_diagonal.dart
    Creamos un nuevo widget header_diagonal.dart, utilizando CustomPainter, creando un Header de forma diagonal.

---
+++ 12_CustomPainter/custompainter_app/lib/src/widgets/header_diagonal.dart
@@ -0,0 +1,56 @@
+//Importaciones Flutter
+import 'package:flutter/material.dart';
+
+//?Creamos el Header Diagonal
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+
+      //todo:Custom Painter
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+//? Creamos una nueva clase, pero extendiendo con el Custom Painter
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Creamos una variable "El lapiz para pintar
+    final lapiz = Paint();
+
+    //?Propiedades del Lapiz
+    lapiz.color = const Color(0xff615AAB);
+
+    //?¿Que tipo de rellenado quiero?
+    //lapiz.style = PaintingStyle.stroke; //Son solo los bordes
+    lapiz.style = PaintingStyle.fill; //Rellena todo
+
+    //?¿Que tan ancho quiero que sea ese lapiz?
+    lapiz.strokeWidth = 2;
+
+    //?¿Como queremos mover el lapiz?
+    final path = Path();
+
+    //Dibujar con el path y con el lapiz
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.28);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    //?Le decimos al canvas que dibuje el Path, usando el lapiz
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
